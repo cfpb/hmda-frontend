74d8620a900028818e1a80ced76e1d2e2470d078
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-11-14 14:18:52 -0800

    documentation updates from https://github.com/cfpb/hmda-documentation/pull/38

--- src/documentation/FigLinks.jsx
+++ src/documentation/FigLinks.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import '../common/images/annual_filing.png'
+import '../common/images/quarterly_filing.png'
 
 const links = {
   2017: [
@@ -9,12 +13,14 @@ const links = {
     <li key="2"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2018-hmda-fig-2018-hmda-rule.pdf">For data collected in 2018 incorporating the 2018 HMDA rule</a></li>
   ],
   2019: [
-    <li key="3"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2019-hmda-fig.pdf">For data collected in 2019</a></li>
+    <li key="3"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2019-hmda-fig.pdf">For data collected in 2019</a></li>,
+    <li key="4"><Link to="/documentation/2019/annual-filing-dates/">Annual HMDA Filing Period Dates</Link></li>
   ],
   2020: [
-    <li key="4"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2020-hmda-fig.pdf">For data collected in 2020</a></li>,
-    <li key="5"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/supplemental-guide-for-quarterly-filers.pdf">Supplemental Guide for Quarterly Filers</a></li>
-
+    <li key="5"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/2020-hmda-fig.pdf">For data collected in 2020</a></li>,
+    <li key="6"><a target="_blank" rel="noopener noreferrer" href="https://s3.amazonaws.com/cfpb-hmda-public/prod/help/supplemental-guide-for-quarterly-filers.pdf">Supplemental Guide for Quarterly Filers</a></li>,
+    <li key="7"><Link to="/documentation/2020/annual-filing-dates/">Annual HMDA Filing Period Dates</Link></li>,
+    <li key="8"><Link to="/documentation/2020/quarterly-filing-dates/">Quarterly HMDA Filing Period Dates</Link></li>
   ]
 }
 
