f37953c25179d867eca3719d0083277f3eb85cd5
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-11-26 17:33:47 -0800

    incorporate https://github.com/cfpb/hmda-platform-ui/pull/1326

--- src/filing/common/CSVDownload.jsx
+++ src/filing/common/CSVDownload.jsx
@@ -3,6 +3,15 @@ import Loading from '../../common/LoadingIcon.jsx'
 
 import './CSVDownload.css'
 
+export function formatPeriod(period) {
+  if (typeof period === 'string') return period
+
+  let { quarter, year } = period
+
+  if (!quarter) return `${year}`
+  return `${year}/quarter/${quarter.toUpperCase()}`
+}
+
 const CSVDownload = props => {
   if (props.submission.id === null) return null
   const { lei, period, sequenceNumber } = props.submission.id
@@ -11,7 +20,7 @@ const CSVDownload = props => {
     <React.Fragment>
       <button
         className="CSVDownload"
-        onClick={props.onDownloadClick(lei, period, sequenceNumber)}
+        onClick={props.onDownloadClick(lei, formatPeriod(period), sequenceNumber)}
         style={props.inline ? { display: 'inline', marginTop: 0 } : null}
       >
         {props.text || 'download the edit report'}
