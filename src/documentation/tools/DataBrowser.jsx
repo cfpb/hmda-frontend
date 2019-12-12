240d263f22ceaad097387cd537a1800b2e5d0909
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2019-12-12 13:36:35 -0800

    fix broken data-browser link in docs

--- src/documentation/tools/DataBrowser.jsx
+++ src/documentation/tools/DataBrowser.jsx
@@ -7,7 +7,7 @@ const links = {
     <li key="0">The HMDA Data Browser currently allows users to filter and download HMDA datasets for 2018 and beyond. Historic data from 2007-2017 is <a target="_blank" rel="noopener noreferrer" href="https://www.consumerfinance.gov/data-research/hmda/historic-data/">available for download here</a>.</li>
   ],
   2018: [
-    <li key="1"><Link to="/2018/data-browser-faq/">Frequently Asked Questions</Link></li>,
+    <li key="1"><Link to="/documentation/2018/data-browser-faq/">Frequently Asked Questions</Link></li>,
     <li key="2"><Link to="/documentation/2018/data-browser-filters/">Available Filters</Link></li>,
     <li key="3"><Link to="/documentation/2018/derived-data-fields/">Derived Data Fields</Link></li>,
     <li key="4"><Link to="/documentation/2018/lar-data-fields/">Public HMDA Data Fields with Values and Definitions</Link></li>,
