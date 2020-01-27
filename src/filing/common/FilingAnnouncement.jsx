5c4ab7a1c6d9b3a375f471dc6ea018ec7a14a04f
meissadia <meissadia@gmail.com>
2020-01-27 11:31:16 -0500

    Sync message format

--- src/filing/common/FilingAnnouncement.jsx
+++ src/filing/common/FilingAnnouncement.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Alert from '../../common/Alert'
 
 export const FilingAnnouncement = ({ data }) => {
-  const { message, type, title } = data
+  const { message, type, heading } = data
   
   return (
     <div className='Announcement'>
-      <Alert type={type} heading={title}>
+      <Alert type={type} heading={heading}>
         <p>{message}</p>
       </Alert>
     </div>
