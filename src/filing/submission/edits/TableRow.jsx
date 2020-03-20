3cc69b1199ed6da2fe560d4bdf748ca6caa0c729
Meissa Dia <meissadia@gmail.com>
2020-03-20 12:54:20 -0600

    S303 Highlight Mismatches

--- src/filing/submission/edits/TableRow.jsx
+++ src/filing/submission/edits/TableRow.jsx
@@ -3,17 +3,50 @@ import PropTypes from 'prop-types'
 
 import EditsTableCell from './TableCell.jsx'
 
+/* Clearly differentiate erroneous "Provided" values */
+export function highlightMismatch(value){
+  if(!value || value.indexOf("Provided") < 0) return value
+
+  let [provided, expected] = value.split(",").map(v => {
+    const [label, val] = v.split(":")
+    return { label, value: val }
+  })
+
+  if(provided.value !== expected.value)
+    return (
+      <>
+        <span className="mismatch">
+          {provided.label}: {provided.value}
+        </span>
+        , {expected.label}: {expected.value}
+      </>
+    )
+  
+  return value
+}
+
 const EditsTableRow = props => {
   if (!props.row) return null
 
   const cells = []
   let cellCount = 0
+  const isS303 = props.edit.edit === 'S303'
 
-  if(props.edit.edit !== 'S303') 
+  if(!isS303) 
     cells.push(<EditsTableCell key={++cellCount} cell={props.row.id} />)
 
   props.row.fields.forEach(field => {
-    cells.push(<EditsTableCell key={++cellCount} cell={field.value} cellName={field.name} />)
+    let cellValue = isS303
+      ? highlightMismatch(field && field.value)
+      : field.value
+      
+    cells.push(
+      <EditsTableCell
+        key={++cellCount}
+        cell={cellValue}
+        cellName={field.name}
+      />
+    )
   })
 
   return <tr>{cells}</tr>
