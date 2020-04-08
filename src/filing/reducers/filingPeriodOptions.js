94494eec36f3eaef7e02dce797a9795c2f1e5e09
Meissa Dia <meissadia@gmail.com>
2020-04-08 13:54:33 -0600

    add filingPeriodOption reducer

---
+++ src/filing/reducers/filingPeriodOptions.js
@@ -0,0 +1,20 @@
+const defaultState = { options: [] }
+
+export default (state = defaultState, action) => {
+  switch (action.type) {
+    case "RECEIVE_PERIOD_OPTIONS":
+      return {
+        isFetching: false,
+        fetched: true,
+        options: action.options,
+      }
+    case "REQUEST_PERIOD_OPTIONS":
+      return {
+        options: [],
+        isFetching: true,
+        fetched: false,
+      }
+    default:
+      return state
+  }
+}
