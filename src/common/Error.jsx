2e6f2e33031f349a43059d8d9022b1dc9fbb5aff
Meissa Dia <meissadia@gmail.com>
2020-04-23 17:58:08 -0600

    Update common Error message component

--- src/common/Error.jsx
+++ src/common/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Alert from './Alert'
 
 const Error = ({error}) => {
-  const standard = <p>Sorry, we were unable to complete your request. We have been notified of the issue and are working to resolve it. Please try again later.</p>
+  const standard = <p>Sorry, we were unable to complete your request. Please try again later. If the problem persists, please contact <a href="mailto:hmdahelp@cfpb.gov">HMDA Help</a>. </p>
   const rateLimited = <p>Sorry, we are currently serving a high volume of requests. Please try again later.</p>
   return (
     <div style={{marginTop: '4px'}} className="Error">
