78e0726993f08291558e2b3cc5f9d78c9bea21aa
Meissa Dia <meissadia@gmail.com>
2020-06-11 13:23:45 -0600

    Add 2019 Modified LAR Report test

--- cypress/integration/data-publication/ModifiedLAR.spec.js
+++ cypress/integration/data-publication/ModifiedLAR.spec.js
@@ -1,6 +1,6 @@
 import { onlyOn } from '@cypress/skip-test'
 import { isBeta } from "../../support/helpers"
-const { HOST, ACTION_DELAY, TEST_DELAY } = Cypress.env()
+const { HOST } = Cypress.env()
 
 onlyOn(isBeta(HOST), () => {
   describe("Modified LAR", function() {
@@ -10,29 +10,49 @@ onlyOn(isBeta(HOST), () => {
 
 onlyOn(!isBeta(HOST), () => {
   describe("Modified LAR", function() {
+    it.only("Searches and finds correct link for 2019", function() {
+      cy.viewport(1680, 867)
+      cy.visit(`${HOST}/data-publication/modified-lar/2019`)
+  
+      cy.get("#institution-name").click()
+      cy.get("#institution-name").type("cypress")
+
+      cy.get("#main-content > .SearchList > h4").contains("1 results found")
+      cy.get("#main-content > .SearchList > .Results > li > p").contains("549300I4IUWMEMGLST06")
+      cy.get("#main-content > .SearchList > .Results > li > .font-small").then(
+        $el => {
+          expect($el).to.have.text("Download Modified LAR ")
+          expect($el).to.have.attr(
+            "href",
+            "https://s3.amazonaws.com/cfpb-hmda-public/prod/modified-lar/2019/549300I4IUWMEMGLST06.txt"
+          )          
+        }
+      )
+  
+      // TODO: Uncomment once "with Header" is available for 2019
+      // cy.get("#inclHeader").click()
+      // cy.get("#inclHeader").check("true")
+
+      // cy.get("#main-content > .SearchList > .Results > li > .font-small").should(
+      //   $el => {
+      //     expect($el).to.have.text("Download Modified LAR with Header")
+      //     expect($el).to.have.attr(
+      //       "href",
+      //       "https://s3.amazonaws.com/cfpb-hmda-public/prod/modified-lar/2019/header/549300I4IUWMEMGLST06_header.txt"
+      //     )
+      //   }
+      // )
+    })
+
     it("Searches and finds correct link for 2018", function() {
       cy.viewport(1680, 867)
-  
       cy.visit(`${HOST}/data-publication/modified-lar/2018`)
   
-      cy.get(
-        "#main-content > .SearchList > form > div > #institution-name"
-      ).click()
-  
-      cy.get("#main-content > .SearchList > form > div > #institution-name").type(
-        "cypress"
-      )
+      cy.get("#institution-name").click()
+      cy.get("#institution-name").type("cypress")
 
-      cy.wait(ACTION_DELAY)
-  
-      cy.get("#root > .App > #main-content > .SearchList > h4").contains(
-        "1 results found"
-      )
-  
-      cy.get("#main-content > .SearchList > .Results > li > p").contains(
-        "549300I4IUWMEMGLST06"
-      )
-  
+      cy.get("#main-content > .SearchList > h4").contains("1 results found")
+      cy.get("#main-content > .SearchList > .Results > li > p").contains("549300I4IUWMEMGLST06")
       cy.get("#main-content > .SearchList > .Results > li > .font-small").then(
         $el => {
           expect($el).to.have.text("Download Modified LAR ")
@@ -43,12 +63,9 @@ onlyOn(!isBeta(HOST), () => {
         }
       )
   
-      cy.get(".App > #main-content > .SearchList > p > #inclHeader").click()
-  
-      cy.get(".App > #main-content > .SearchList > p > #inclHeader").check("true")
-      
-      cy.wait(ACTION_DELAY)
-  
+      cy.get("#inclHeader").click()
+      cy.get("#inclHeader").check("true")
+
       cy.get("#main-content > .SearchList > .Results > li > .font-small").should(
         $el => {
           expect($el).to.have.text("Download Modified LAR with Header")
@@ -62,23 +79,12 @@ onlyOn(!isBeta(HOST), () => {
   
     it("Searches and finds correct link for 2017", function() {
       cy.viewport(1680, 867)
-
       cy.visit(`${HOST}/data-publication/modified-lar/2017`)
 
-      cy.get(
-        "#main-content > .SearchList > form > div > #institution-name"
-      ).click()
+      cy.get("#institution-name").click()
+      cy.get("#institution-name").type("cypress")
 
-      cy.get(
-        "#main-content > .SearchList > form > div > #institution-name"
-      ).type("cypress")
-
-      cy.wait(ACTION_DELAY)
-
-      cy.get("#main-content > .SearchList > .Results > li > p").contains(
-        "729178"
-      )
-
+      cy.get("#main-content > .SearchList > .Results > li > p").contains("729178")
       cy.get("#main-content > .SearchList > .Results > li > .font-small").then(
         $el => {
           expect($el).to.have.text("Download Modified LAR ")
@@ -89,13 +95,8 @@ onlyOn(!isBeta(HOST), () => {
         }
       )
 
-      cy.get(".App > #main-content > .SearchList > p > #inclHeader").click()
-
-      cy.get(".App > #main-content > .SearchList > p > #inclHeader").check(
-        "true"
-      )
-
-      cy.wait(ACTION_DELAY)
+      cy.get("#inclHeader").click()
+      cy.get("#inclHeader").check("true")
 
       cy.get("#main-content > .SearchList > .Results > li > .font-small").then(
         $el => {
@@ -106,22 +107,15 @@ onlyOn(!isBeta(HOST), () => {
           )             
         }
       )
-
-      cy.wait(TEST_DELAY)
     })
   
    it("Has the right documentation link", function() {
      cy.viewport(1680, 867)
-  
      cy.visit(`${HOST}/data-publication/modified-lar/2018`)
-  
      cy.get(".App > #main-content > .heading > p > a").click()
-  
      cy.get("#root > .App > #main-content > .DocSection > h2").contains(
        "Modified LAR"
      )
-
-     cy.wait(TEST_DELAY)
    })  
   })
 })
