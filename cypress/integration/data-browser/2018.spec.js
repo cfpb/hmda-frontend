17c2ddb7508e80f0f1293f8fcca018ebe1440b3b
Meissa Dia <meissadia@gmail.com>
2020-06-24 14:24:03 -0600

    [Cypress] Skip DB 2019 and 2018 tests in environments where they are not running

--- cypress/integration/data-browser/2018.spec.js
+++ cypress/integration/data-browser/2018.spec.js
@@ -4,166 +4,170 @@ const { HOST } = Cypress.env()
 const dbUrl = dbURL.bind(null, HOST)
 
 describe('Data Browser 2018', function () {
-  it('State/Institution/PropertyType', function () {
-    cy.viewport(1000, 940)
-    cy.visit(dbUrl('2018?category=states'))
+  if(!isBeta(HOST) || (isBeta(HOST) && !isProd(HOST)) ){
+    it('State/Institution/PropertyType', function () {
+      cy.viewport(1000, 940)
+      cy.visit(dbUrl('2018?category=states'))
 
-    // Select Geography
-    openSelector('#ItemSelector')
-    cy.get('#ItemSelector').type('alabama{enter}')
-    cy.get('#ItemSelector').type('alaska{enter}')
-    cy.url().should('include', '?category=states&items=AL,AK')
+      // Select Geography
+      openSelector('#ItemSelector')
+      cy.get('#ItemSelector').type('alabama{enter}')
+      cy.get('#ItemSelector').type('alaska{enter}')
+      cy.url().should('include', '?category=states&items=AL,AK')
 
-    // Select Institutions
-    openSelector('#lei-item-select')
-    cy.get('#lei-item-select').type('bank of america{enter}')
-    cy.get('#lei-item-select').type('chase bank{enter}')
-    cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
+      // Select Institutions
+      openSelector('#lei-item-select')
+      cy.get('#lei-item-select').type('bank of america{enter}')
+      cy.get('#lei-item-select').type('chase bank{enter}')
+      cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
 
-    // Variables
-    openSelector('#VariableSelector')
-    cy.get('.SelectWrapper #react-select-5-option-10').click()
-    cy.get('.border > :nth-child(2) :checkbox').check('on')
-    cy.url().should('include', '&dwelling_categories=Single%20Family%20(1-4%20Units)%3ASite-Built')
+      // Variables
+      openSelector('#VariableSelector')
+      cy.get('.SelectWrapper #react-select-5-option-10').click()
+      cy.get('.border > :nth-child(2) :checkbox').check('on')
+      cy.url().should('include', '&dwelling_categories=Single%20Family%20(1-4%20Units)%3ASite-Built')
 
-    // View Summary Table
-    cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
-    cy.get('.Aggregations').should('exist')
-    cy.get('.Aggregations :nth-child(1) > .sublist > li').should('have.text', 'ALABAMA, ALASKA')
-    cy.get('.Aggregations :nth-child(2) > .sublist > li').then($li => {
-      let text = $li.text().toLowerCase()
-      cy.wrap(text).should('contain', 'bank of america')
-      cy.wrap(text).should('contain', 'chase bank')
+      // View Summary Table
+      cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
+      cy.get('.Aggregations').should('exist')
+      cy.get('.Aggregations :nth-child(1) > .sublist > li').should('have.text', 'ALABAMA, ALASKA')
+      cy.get('.Aggregations :nth-child(2) > .sublist > li').then($li => {
+        let text = $li.text().toLowerCase()
+        cy.wrap(text).should('contain', 'bank of america')
+        cy.wrap(text).should('contain', 'chase bank')
+      })
+      cy.get('.Aggregations :nth-child(3) > .sublist > li').should('have.text', 'Single Family (1-4 Units):Site-Built')
+      cy.get('.Error').should('not.exist')
+
+      // Test validity of download link
+      if(isProd(HOST) || isBeta(HOST)){
+        cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
+          assert.isTrue(status, 'Has valid download link.')
+        })
+      }
     })
-    cy.get('.Aggregations :nth-child(3) > .sublist > li').should('have.text', 'Single Family (1-4 Units):Site-Built')
-    cy.get('.Error').should('not.exist')
 
-    // Test validity of download link
-    if(isProd(HOST) || isBeta(HOST)){
-      cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
-        assert.isTrue(status, 'Has valid download link.')
+    it('Nationwide/Institution/LienStatus', function () {
+      cy.viewport(1000, 940)
+      cy.visit(dbUrl('2018?category=nationwide'))
+
+      // Select Institutions
+      openSelector('#lei-item-select')
+      cy.get('#lei-item-select').type('bank of america{enter}')
+      cy.get('#lei-item-select').type('chase bank{enter}')
+      cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
+
+      // Variables
+      openSelector('#VariableSelector')
+      cy.get('#VariableSelector').type('lien{enter}')
+      cy.get('#lien_statuses1').check('on')
+      cy.url().should('include', '&lien_statuses=1')
+
+      // View Summary Table
+      cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
+      cy.get('.Aggregations').should('exist')
+      cy.get('.Aggregations :nth-child(1) > .sublist > li').then($li => {
+        let text = $li.text().toLowerCase()
+        cy.wrap(text).should('contain', 'bank of america')
+        cy.wrap(text).should('contain', 'chase bank')
       })
-    }
-  })
+      cy.get('.Aggregations :nth-child(2) > .sublist > li').should('have.text', '1 - Secured By First Lien')
+      cy.get('.Error').should('not.exist')
 
-  it('Nationwide/Institution/LienStatus', function () {
-    cy.viewport(1000, 940)
-    cy.visit(dbUrl('2018?category=nationwide'))
-
-    // Select Institutions
-    openSelector('#lei-item-select')
-    cy.get('#lei-item-select').type('bank of america{enter}')
-    cy.get('#lei-item-select').type('chase bank{enter}')
-    cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
-
-    // Variables
-    openSelector('#VariableSelector')
-    cy.get('#VariableSelector').type('lien{enter}')
-    cy.get('#lien_statuses1').check('on')
-    cy.url().should('include', '&lien_statuses=1')
-
-    // View Summary Table
-    cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
-    cy.get('.Aggregations').should('exist')
-    cy.get('.Aggregations :nth-child(1) > .sublist > li').then($li => {
-      let text = $li.text().toLowerCase()
-      cy.wrap(text).should('contain', 'bank of america')
-      cy.wrap(text).should('contain', 'chase bank')
+      // Test validity of download link
+      if(isProd(HOST) || isBeta(HOST)){
+        cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
+          assert.isTrue(status, 'Has valid download link.')
+        })
+      }
     })
-    cy.get('.Aggregations :nth-child(2) > .sublist > li').should('have.text', '1 - Secured By First Lien')
-    cy.get('.Error').should('not.exist')
 
-    // Test validity of download link
-    if(isProd(HOST) || isBeta(HOST)){
-      cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
-        assert.isTrue(status, 'Has valid download link.')
+    it('County/Institution/Action&Purpose', function () {
+      cy.viewport(1000, 940)
+      cy.visit(dbUrl('2018?category=counties'))
+
+      // Select Geography
+      openSelector('#ItemSelector')
+      cy.get('#react-select-3-option-0').click()
+      openSelector('#ItemSelector')
+      cy.get('#react-select-3-option-0').click()
+      cy.url().should('include', '?category=counties&items=01001,01003')
+
+      // Select Institutions
+      openSelector('#lei-item-select')
+      cy.get('#lei-item-select').type('bank of america{enter}')
+      cy.get('#lei-item-select').type('chase bank{enter}')
+      cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
+
+      // Variables
+      openSelector('#VariableSelector')
+      cy.get('#VariableSelector').type('total units{enter}')
+      cy.get('#total_units25-49').check('on')
+      cy.url().should('include', '&total_units=25-49')
+
+      // View Summary Table
+      cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
+      cy.get('.Aggregations').should('exist')
+      cy.get('.Aggregations :nth-child(1) > .sublist > li').should('have.text', 'AUTAUGA COUNTY, BALDWIN COUNTY')
+      cy.get('.Aggregations :nth-child(2) > .sublist > li').then($li => {
+        let text = $li.text().toLowerCase()
+        cy.wrap(text).should('contain', 'bank of america')
+        cy.wrap(text).should('contain', 'chase bank')
       })
-    }
-  })
+      cy.get('.Aggregations :nth-child(3) > .sublist > li').should('have.text', '25-49')
+      cy.get('.Error').should('not.exist')
 
-  it('County/Institution/Action&Purpose', function () {
-    cy.viewport(1000, 940)
-    cy.visit(dbUrl('2018?category=counties'))
-
-    // Select Geography
-    openSelector('#ItemSelector')
-    cy.get('#react-select-3-option-0').click()
-    openSelector('#ItemSelector')
-    cy.get('#react-select-3-option-0').click()
-    cy.url().should('include', '?category=counties&items=01001,01003')
-
-    // Select Institutions
-    openSelector('#lei-item-select')
-    cy.get('#lei-item-select').type('bank of america{enter}')
-    cy.get('#lei-item-select').type('chase bank{enter}')
-    cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
-
-    // Variables
-    openSelector('#VariableSelector')
-    cy.get('#VariableSelector').type('total units{enter}')
-    cy.get('#total_units25-49').check('on')
-    cy.url().should('include', '&total_units=25-49')
-
-    // View Summary Table
-    cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
-    cy.get('.Aggregations').should('exist')
-    cy.get('.Aggregations :nth-child(1) > .sublist > li').should('have.text', 'AUTAUGA COUNTY, BALDWIN COUNTY')
-    cy.get('.Aggregations :nth-child(2) > .sublist > li').then($li => {
-      let text = $li.text().toLowerCase()
-      cy.wrap(text).should('contain', 'bank of america')
-      cy.wrap(text).should('contain', 'chase bank')
+      // Test validity of download link
+      if(isProd(HOST) || isBeta(HOST)){
+        cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
+          assert.isTrue(status, 'Has valid download link.')
+        })
+      }
     })
-    cy.get('.Aggregations :nth-child(3) > .sublist > li').should('have.text', '25-49')
-    cy.get('.Error').should('not.exist')
 
-    // Test validity of download link
-    if(isProd(HOST) || isBeta(HOST)){
-      cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
-        assert.isTrue(status, 'Has valid download link.')
-      })
-    }
-  })
+    it('MSA/Institution/PropertyType', function () {
+      cy.viewport(1000, 940)
+      cy.visit(dbUrl('2018?category=msamds'))
 
-  it('MSA/Institution/PropertyType', function () {
-    cy.viewport(1000, 940)
-    cy.visit(dbUrl('2018?category=msamds'))
+      // Select Geography
+      openSelector('#ItemSelector')
+      cy.get('#react-select-3-option-0').click()
+      openSelector('#ItemSelector')
+      cy.get('#react-select-3-option-0').click()
+      cy.url().should('include', '?category=msamds&items=11500,12220')
 
-    // Select Geography
-    openSelector('#ItemSelector')
-    cy.get('#react-select-3-option-0').click()
-    openSelector('#ItemSelector')
-    cy.get('#react-select-3-option-0').click()
-    cy.url().should('include', '?category=msamds&items=11500,12220')
+      // Select Institutions
+      openSelector('#lei-item-select')
+      cy.get('#lei-item-select').type('bank of america{enter}')
+      cy.get('#lei-item-select').type('chase bank{enter}')
+      cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
 
-    // Select Institutions
-    openSelector('#lei-item-select')
-    cy.get('#lei-item-select').type('bank of america{enter}')
-    cy.get('#lei-item-select').type('chase bank{enter}')
-    cy.url().should('include', 'leis=B4TYDEB6GKMZO031MB27,7H6GLXDRUGQFU57RNE97')
+      // Variables
+      openSelector('#VariableSelector')
+      cy.get('.SelectWrapper #react-select-5-option-1').click()
+      cy.get('#loan_types1').check('on')
+      cy.url().should('include', '&loan_types=1')
 
-    // Variables
-    openSelector('#VariableSelector')
-    cy.get('.SelectWrapper #react-select-5-option-1').click()
-    cy.get('#loan_types1').check('on')
-    cy.url().should('include', '&loan_types=1')
+      // View Summary Table
+      cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
+      cy.get('.Aggregations').should('exist')
+      cy.get('.Aggregations :nth-child(1) > .sublist > li').should('have.text',"11500\u00A0-\u00A0ANNISTON-OXFORD-JACKSONVILLE, 12220\u00A0-\u00A0AUBURN-OPELIKA")
+      cy.get('.Aggregations :nth-child(2) > .sublist > li').then($li => {
+        let text = $li.text().toLowerCase()
+        cy.wrap(text).should('contain', 'bank of america')
+        cy.wrap(text).should('contain', 'chase bank')
+      })    
+      cy.get('.Aggregations :nth-child(3) > .sublist > li').should('have.text', '1 - Conventional')
+      cy.get('.Error').should('not.exist')
 
-    // View Summary Table
-    cy.get('body > #root > .DataBrowser > .Geography > .secondary').click()
-    cy.get('.Aggregations').should('exist')
-    cy.get('.Aggregations :nth-child(1) > .sublist > li').should('have.text',"11500\u00A0-\u00A0ANNISTON-OXFORD-JACKSONVILLE, 12220\u00A0-\u00A0AUBURN-OPELIKA")
-    cy.get('.Aggregations :nth-child(2) > .sublist > li').then($li => {
-      let text = $li.text().toLowerCase()
-      cy.wrap(text).should('contain', 'bank of america')
-      cy.wrap(text).should('contain', 'chase bank')
-    })    
-    cy.get('.Aggregations :nth-child(3) > .sublist > li').should('have.text', '1 - Conventional')
-    cy.get('.Error').should('not.exist')
-
-    // Test validity of download link
-    if(isProd(HOST) || isBeta(HOST)){
-      cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
-        assert.isTrue(status, 'Has valid download link.')
-      })
-    }
-  })
+      // Test validity of download link
+      if(isProd(HOST) || isBeta(HOST)){
+        cy.get('.QueryButton:first').dataUrl().then(({ status }) => {
+          assert.isTrue(status, 'Has valid download link.')
+        })
+      }
+    })
+  } else {
+    it(`does not run on host: ${HOST}`)
+  }
 })
