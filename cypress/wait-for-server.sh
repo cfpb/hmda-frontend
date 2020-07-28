ad831d5eaad8313e0d9c23904ba7dffd72a5328b
Meissa Dia <meissadia@gmail.com>
2020-07-27 21:34:10 -0600

    Mock auth response

--- cypress/wait-for-server.sh
+++ cypress/wait-for-server.sh
@@ -1,5 +1,5 @@
 # Wait for the Filing app to be reachable
-while ! curl -s "0:0:0:0:0:0:0:0:8080" >/dev/null; do
+while ! curl -s "localhost:8080" >/dev/null; do
     echo "Waiting for localhost:8080..."
     sleep 10s
 done
