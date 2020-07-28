d2c425a77e0bebc12656a6350bda51627e351bac
Meissa Dia <meissadia@gmail.com>
2020-07-27 20:40:44 -0600

    Wait for Filing app on port 8080

--- cypress/wait-for-server.sh
+++ cypress/wait-for-server.sh
@@ -1,4 +1,5 @@
-while ! curl -s "0:0:0:0:0:0:0:0:$APP_PORT" >/dev/null; do
-    echo "Waiting for localhost:$APP_PORT..."
+# Wait for the Filing app to be reachable
+while ! curl -s "0:0:0:0:0:0:0:0:8080" >/dev/null; do
+    echo "Waiting for localhost:8080..."
     sleep 10s
 done
