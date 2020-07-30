e29134f665a888cce67c0b1642f3e0a876e592e7
Meissa Dia <meissadia@gmail.com>
2020-07-30 14:49:40 -0600

    Limit wait-for-server script to maximum 5mins

--- cypress/wait-for-server.sh
+++ cypress/wait-for-server.sh
@@ -1,5 +1,12 @@
+i=0
+
 # Wait for the Filing app to be reachable
 while ! curl -s "localhost:8080" >/dev/null; do
-    echo "Waiting for localhost:8080..."
+    ((i = i + 1))
+    if [[ "$i" -gt 30 ]]; then
+        echo "Error: Timed out waiting for the HMDA Platform to load! "
+        exit 1
+    fi
+    echo "Waiting for the HMDA Platform to load on localhost:8080..."
     sleep 10s
 done
