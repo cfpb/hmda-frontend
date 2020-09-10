1250d16cf3d4013540fa0f610bda928135c5c4f9
Meissa Dia <meissadia@gmail.com>
2020-09-10 13:44:21 -0600

    Group Report and Dataset products in listings

--- src/data-publication/constants/publications-by-year.js
+++ src/data-publication/constants/publications-by-year.js
@@ -27,12 +27,6 @@ const publications =  {
         
       },
       {
-        headingLink: narLink(2019),
-        headingText: "National Aggregate Reports",
-        paragraphText: "These reports summarize nationwide lending activity. They indicate the number and dollar amounts of loan applications, cross-tabulated by loan, borrower and geographic characteristics.",
-        
-      },
-      {
         headingLink: snapLink(2019),
         headingText: "Snapshot National Loan-Level Dataset",
         paragraphText: "The snapshot files contain the national HMDA datasets as of a fixed date for all HMDA reporters, as modified by the Bureau to protect applicant and borrower privacy.",
@@ -50,11 +44,6 @@ const publications =  {
       paragraphText: "The modified LAR provides loan-level data for an individual financial institution, as modified by the Bureau to protect applicant and borrower privacy.",
     },
     {
-      headingLink: finLink(2018),
-      headingText: "Final National Loan-Level Dataset",
-      paragraphText: "Details pending...",
-    },
-    {
       headingLink: drLink(2018),
       headingText: "Disclosure Reports",
       paragraphText: "These reports summarize lending activity for individual institutions, both nationwide and by MSA/MD.",
@@ -66,10 +55,9 @@ const publications =  {
       
     },
     {
-      headingLink: narLink(2018),
-      headingText: "National Aggregate Reports",
-      paragraphText: "These reports summarize nationwide lending activity. They indicate the number and dollar amounts of loan applications, cross-tabulated by loan, borrower and geographic characteristics.",
-      
+      headingLink: finLink(2018),
+      headingText: "Final National Loan-Level Dataset",
+      paragraphText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quaerat non deleniti! Voluptatem esse corrupti eligendi animi suscipit, at explicabo.",
     },
     {
       headingLink: snapLink(2018),
@@ -89,16 +77,6 @@ const publications =  {
       paragraphText: "The modified LAR provides loan-level data for an individual financial institution, as modified by the Bureau to protect applicant and borrower privacy.",
     },
     {
-      headingLink: ultLink(2017),
-      headingText: "Ultimate National Loan-Level Dataset",
-      paragraphText: "Details pending...",
-    },
-    {
-      headingLink: finLink(2017),
-      headingText: "Final National Loan-Level Dataset",
-      paragraphText: "Details pending...",
-    },
-    {
       headingLink: drLink(2017),
       headingText: "Disclosure Reports",
       paragraphText: "These reports summarize lending activity for individual institutions, both nationwide and by MSA/MD.",
@@ -114,6 +92,16 @@ const publications =  {
       headingText: "National Aggregate Reports",
       paragraphText: "These reports summarize nationwide lending activity. They indicate the number and dollar amounts of loan applications, cross-tabulated by loan, borrower and geographic characteristics.",
       
+    },
+    {
+      headingLink: ultLink(2017),
+      headingText: "Ultimate National Loan-Level Dataset",
+      paragraphText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero a consectetur expedita omnis quasi quod, dolorem provident ducimus soluta eum praesentium nobis minus odio tempora quae aperiam? Magnam, deleniti possimus.",
+    },
+    {
+      headingLink: finLink(2017),
+      headingText: "Final National Loan-Level Dataset",
+      paragraphText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quaerat non deleniti! Voluptatem esse corrupti eligendi animi suscipit, at explicabo.",
     },
     {
       headingLink: snapLink(2017),
