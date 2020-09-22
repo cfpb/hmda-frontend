c136376b445a9d1fc038cc9575c5f9306c6aa771
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2020-09-22 12:54:03 -0700

    point to new s3 location

--- src/data-browser/maps/filterUtils.jsx
+++ src/data-browser/maps/filterUtils.jsx
@@ -1,6 +1,6 @@
 function getFile(g, v, val) {
   const dec = decodeURIComponent(val)
-  return `https://s3.amazonaws.com/cfpb-hmda-public/prod/data-browser/filter-data/${g}-${v}-${dec.replace(/[^a-z0-9]/ig, '-').toLowerCase()}.json`
+  return `https://s3.amazonaws.com/cfpb-hmda-public/prod/data-browser/filter-data/2018/${g}-${v}-${dec.replace(/[^a-z0-9]/ig, '-').toLowerCase()}.json`
 }
 
 export function fetchFilterData(geography, variable, value) {
