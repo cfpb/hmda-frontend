825448d9de6f7c4936f628919c18eeed31a018eb
Meissa Dia <meissadia@gmail.com>
2020-09-23 16:15:59 -0600

    Extend wait time for platform/frontend on CI builds

--- cypress/wait-for-server.sh
+++ cypress/wait-for-server.sh
@@ -1,14 +1,16 @@
+MAX_ITERATIONS=60
+WAIT_TIME=10s
 i=0
 
 # Wait for the Filing app to be reachable
 while ! curl -s "localhost:8080" >/dev/null; do
     ((i = i + 1))
-    if [[ "$i" -gt 30 ]]; then
+    if [[ "$i" -gt $MAX_ITERATIONS ]]; then
         echo "Error: Timed out waiting for the HMDA Platform to load! "
         exit 1
     fi
     echo "Waiting for the HMDA Platform to load on localhost:8080..."
-    sleep 10s
+    sleep $WAIT_TIME
 done
 
 i=0
@@ -16,10 +18,10 @@ i=0
 # Wait for the Frontend app to be reachable
 while ! curl -s "localhost:3000" >/dev/null; do
     ((i = i + 1))
-    if [[ "$i" -gt 30 ]]; then
+    if [[ "$i" -gt $MAX_ITERATIONS ]]; then
         echo "Error: Timed out waiting for the HMDA Frontend to load! "
         exit 1
     fi
     echo "Waiting for the HMDA Frontend to load on localhost:3000..."
-    sleep 10s
+    sleep $WAIT_TIME
 done
