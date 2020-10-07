667084cd9ea7f31cc33830d82097fae66fd2c1d1
Meissa Dia <meissadia@gmail.com>
2020-10-07 15:57:12 -0600

    Add Institution API to travis.yml and wait-for-server script

--- cypress/wait-for-server.sh
+++ cypress/wait-for-server.sh
@@ -25,3 +25,16 @@ while ! curl -s "localhost:3000" >/dev/null; do
     echo "Waiting for the HMDA Frontend to load on localhost:3000..."
     sleep $WAIT_TIME
 done
+
+i=0
+
+# Wait for the Institution API to be reachable
+while ! curl -s "localhost:9092" >/dev/null; do
+    ((i = i + 1))
+    if [[ "$i" -gt 30 ]]; then
+        echo "Error: Timed out waiting for the HMDA Institutions API to load! "
+        exit 1
+    fi
+    echo "Waiting for the HMDA Institutions API to load on localhost:9092..."
+    sleep 10s
+done
