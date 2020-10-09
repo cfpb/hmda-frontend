6b13f5cf6a1bc76867281a53226c5b780bc0aa4c
Meissa Dia <meissadia@gmail.com>
2020-10-09 13:01:47 -0600

    Rework wait script and try starting dev servers sequentially

--- cypress/wait-for-server.sh
+++ cypress/wait-for-server.sh
@@ -1,40 +1,57 @@
-MAX_ITERATIONS=60
-WAIT_TIME=10s
+API=$1
+MAX=90
 i=0
 
-# Wait for the Filing app to be reachable
-while ! curl -s "localhost:8080" >/dev/null; do
-    ((i = i + 1))
-    if [[ "$i" -gt 90 ]]; then
-        echo "Error: Timed out waiting for the HMDA Platform to load! "
-        exit 1
-    fi
-    echo "Waiting for the HMDA Platform to load on localhost:8080..."
-    sleep $WAIT_TIME
-done
-
-i=0
-
-# Wait for the Frontend app to be reachable
-while ! curl -s "localhost:3000" >/dev/null; do
-    ((i = i + 1))
-    if [[ "$i" -gt 90 ]]; then
-        echo "Error: Timed out waiting for the HMDA Frontend to load! "
-        exit 1
-    fi
-    echo "Waiting for the HMDA Frontend to load on localhost:3000..."
-    sleep $WAIT_TIME
-done
-
-i=0
-
-# Wait for the Institution API to be reachable
-while ! curl -s "localhost:8282" >/dev/null; do
-    ((i = i + 1))
-    if [[ "$i" -gt 90 ]]; then
-        echo "Error: Timed out waiting for the HMDA Institutions API to load! "
-        exit 1
-    fi
-    echo "Waiting for the HMDA Institutions API to load on localhost:8282..."
-    sleep 10s
-done
+if [[ $API = 'help' ]]
+then
+    echo ""
+    echo "Wait for Server"
+    echo ""
+    echo "  Polling script to pause while waiting for a development server to become available"
+    echo ""
+    echo "  Usage:"
+    echo "   $> ./wait-for-server.sh <platform | institutions | frontend>"
+    echo ""
+elif [[ $API = 'institutions' ]]
+then
+    # Wait for the Institution API to be reachable
+    while ! curl -s "localhost:8282" >/dev/null; do
+        ((i = i + 1))
+        if [[ "$i" -gt "$MAX" ]]
+        then
+            echo "Error: Timed out waiting for the HMDA Institutions API to load! "
+            exit 1
+        fi
+        echo "Waiting for the HMDA Institutions API to load on localhost:8282..."
+        sleep 10s
+    done
+    exit 0
+elif [[ $API = 'platform' ]]
+then
+    # Wait for the Filing app to be reachable
+    while ! curl -s "localhost:8080" >/dev/null; do
+        ((i = i + 1))
+        if [[ "$i" -gt "$MAX" ]]
+        then
+            echo "Error: Timed out waiting for the HMDA Platform to load! "
+            exit 1
+        fi
+        echo "Waiting for the HMDA Platform to load on localhost:8080..."
+        sleep 10s
+    done
+    exit 0
+elif [[ $API = 'frontend' ]]
+then
+    # Wait for the Frontend app to be reachable
+    while ! curl -s "localhost:3000" >/dev/null; do
+        ((i = i + 1))
+        if [[ "$i" -gt "$MAX" ]]
+        then
+            echo "Error: Timed out waiting for the HMDA Frontend to load! "
+            exit 1
+        fi
+        echo "Waiting for the HMDA Frontend to load on localhost:3000..."
+        sleep 10s
+    done
+    exit 0
+fi
