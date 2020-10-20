fe2acba6d9bc6ccc8d8df847bd4278ffd15be43a
Barak Stout <barakstout@gmail.com>
2020-10-20 10:35:28 -0400

    updates to cypress notifications

--- cypress/docker-runner.sh
+++ cypress/docker-runner.sh
@@ -2,10 +2,11 @@
 
 # Script that will post a message to a predefined webhook when cypress is done
 
-RESULTS=":starwars-darth: Cypress Has Failed!"
-$(yarn cypress run > output.txt) && RESULTS=":thumbs-up-bb8: No Failing specs."
-RES=$(awk '/Run Finished/ {p=1}; p==1 {print}' output.txt)
-curl -i -X POST -H 'Content-Type: application/json' \
-	 -d "{\"text\": \"CYPRESS_HOST: ${CYPRESS_HOST} Complete.\n${RESULTS} See results below:\n\`\`\`\n${RES}\n\`\`\`\"}" \
-	 $CYPRESS_WEB_HOOK
+FAILED=$(yarn cypress run > output.txt)
+if [ $FAILED = 0 ]; then
+	curl -i -X POST -H 'Content-Type: application/json' -d "{\"text\": \"CYPRESS_HOST: ${CYPRESS_HOST} Complete. :thumbs-up-bb8: No Failing specs.\"}" $CYPRESS_WEB_HOOK
+else
+	curl -i -X POST -H 'Content-Type: application/json' -d "{\"text\": \"CYPRESS_HOST: ${CYPRESS_HOST} Complete.\n:starwars-darth: Cypress Has Failed!\nrun \`kubectl logs \$(kubectl get pods -n monitoring --sort-by=.status.startTime | grep 'hmda-cypress-${CYPRESS_ENV}' | tail -n 1 | awk 'NR==1{print \$1}') -n monitoring\` to see the logs for the last run\" }"  $CYPRESS_WEB_HOOK
+	cat output.txt
+fi
 rm output.txt
