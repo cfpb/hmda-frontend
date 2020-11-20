f708cf4fc082fbd03d4369980740ab531c549ae8
Meissa Dia <meissadia@gmail.com>
2020-11-19 18:47:49 -0700

    Add ResultCount tests

--- cypress/integration/data-publication/ChangeLog.spec.js
+++ cypress/integration/data-publication/ChangeLog.spec.js
@@ -8,37 +8,44 @@ describe('Change Log', () => {
     it('Loads config from query string', () => {
       cy.visit(`${HOST}/data-publication/updates?type=release,notice&product=documentation,apps&keywords=2020,tool`)
       
-      // Validate pills get set on load
+      cy.log(">> Validate pills get set on load")
       EXPECTED_SELECTED_PILLS.forEach(pillClass => {
         cy.get(`#filter-bar .pill.selected.${pillClass}`)
       })
 
-      // Check keyword filter works
+      cy.log(">> Check keyword filter works")
       cy.get('.change-row').should('have.length', 2)
+      cy.get('.result-count .body').should('contain', 'Showing ' + 1 + ' out of')
       cy.get('.change-row').find('.highlighted').its('length').should('be.gte', 2)
 
       cy.get('.reset-filters').click()
       cy.get('.change-row').should('have.length', entries.length + 1)
+      cy.get('.result-count').should('not.exist')
 
-      // Changing filters should update the URL
+      cy.log(">> Changing filters should update the URL")
       cy.get('#filter-bar').findByText('notice').click()
       cy.url().should('contain', '?type=notice')
       cy.get('.change-row:not(.header)').should('not.exist')
+      cy.get('.result-count .body').should('contain', 'Showing ' + 0 + ' out of')
 
       cy.get('#filter-bar').findByText('update').click()
       cy.get('#filter-bar').findByText('HMDA Apps').click()
       cy.url().should('contain', '?type=notice,update&product=apps')
       cy.get('.change-row').should('have.length', 5)
+      cy.get('.result-count .body').should('contain', 'Showing ' + 4 + ' out of')
       
-      // Search by Keyword
+      cy.log(">> Search by Keyword")
       cy.findByLabelText('by Change Description').type('2018')
       cy.url().should('contain', '?type=notice,update&product=apps&keywords=2018')
       cy.get('.change-row').should('have.length', 2)
+      cy.get('.result-count .body').should('contain', 'Showing ' + 1 + ' out of')
+      cy.log(">> Highlight keywords")
       cy.get('.change-row .highlighted').should('have.length', 1)
 
-      // Clear keyword search with button
+      cy.log(">> Clear keyword search with button")
       cy.get('.clear-text').click()
       cy.get('.change-row').should('have.length', 5)
+      cy.get('.result-count .body').should('contain', 'Showing ' + 4 + ' out of')
       
       // TODO: Test that entries are sorted by date
       // TODO: Only show entries for the past year
