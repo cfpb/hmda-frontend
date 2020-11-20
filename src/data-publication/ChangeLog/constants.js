e9101178602c3e5c661fa2371d426014c42c2549
Meissa Dia <meissadia@gmail.com>
2020-11-20 12:49:49 -0700

    Move change log data into ChangeLog folder for better grouping.

--- src/data-publication/ChangeLog/constants.js
+++ src/data-publication/ChangeLog/constants.js
@@ -19,7 +19,7 @@ export const CATEGORIES = {
 
 
 export const PUB_CHANGELOG_URL =
-  'https://raw.githubusercontent.com/cfpb/hmda-frontend/master/src/data-publication/constants/publicationChangeLog.json'
+  'https://raw.githubusercontent.com/cfpb/hmda-frontend/master/src/data-publication/ChangeLog/change-log-data.json'
 
 
 export const DEFAULT_FILTERS = {
