d50cf0adf880c5e447a152766febc707499a5c82
Meissa Dia <meissadia@gmail.com>
2020-12-15 16:18:22 -0700

    Add some options for the page description text

--- src/data-browser/maps/MapsGraphs.jsx
+++ src/data-browser/maps/MapsGraphs.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Heading from '../../common/Heading.jsx'
 import MapContainer from './MapContainer.jsx'
-
+import { OptionCarousel } from '../../common/OptionCarousel'
 import './MapsGraphs.css'
+
+const PopularVariableLink = ({ children = 'popular variable', year }) => (
+  <a
+    target='_blank'
+    rel='noopener noreferrer'
+    href={`/documentation/${year}/data-browser-filters/#action_taken`}
+  >
+    {children}
+  </a>
+)
 
 const MapsGraphs = props => {
   return (
@@ -14,8 +24,16 @@ const MapsGraphs = props => {
       <div className='intro'>
         <Heading type={1} headingText='Map of HMDA Data'>
           <p className='lead'>
-            The Map tool gives you the opportunity to visualize cross-selections
-            of HMDA Data variables. For questions/suggestions, contact{' '}
+            The Map tool gives you the opportunity to visualize{' '}
+            <OptionCarousel
+              className='inline'
+              options={[
+                'cross sections of HMDA Data variables.',
+                'cross-selections of HMDA Data variables.',
+                <>subsets of HMDA Data, filtered by <PopularVariableLink year={props.match.params.year}>popular variables</PopularVariableLink>.</>,
+              ]}
+            />{' '}
+            For questions/suggestions, contact{' '}
             <a href='mailto:hmdahelp@cfpb.gov'>hmdahelp@cfpb.gov</a>.
           </p>
         </Heading>
