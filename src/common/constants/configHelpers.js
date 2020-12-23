06117bceabd0ae082ac15437e0579902462a2f8d
Meissa Dia <meissadia@gmail.com>
2020-12-23 10:35:22 -0700

    [FFVT] Make year checking more dynamic, based on env config

--- src/common/constants/configHelpers.js
+++ src/common/constants/configHelpers.js
@@ -1,17 +1,39 @@
 import { splitYearQuarter } from '../../filing/api/utils'
 
-export const getFilingYears = config => {
-  let filingYears = new Set()
-  
-  // Collect all PREVIEW or available (filingPeriods) years
-  let tmpFilingYears = [...config.filingPeriods, ...(config.filingQuarters.PREVIEW || [])]
+const defaultOpts = {
+  withAdmin: true // Include administrative years (PREVIEW) in the returned list
+}
 
+/**
+ * 
+ * @param {Object} config Environment configuration
+ * @param {Object} options See defaultOps for all options
+ */
+export const getFilingYears = (config, options = defaultOpts) => {
+  let filingYears = new Set()
+  const { withAdmin } = options
+
+  const prevArry = withAdmin ? (config.filingQuarters.PREVIEW || []) : []
+  let tmpFilingYears = [...config.filingPeriods, ...prevArry]
+  
   // Unique
   tmpFilingYears.forEach(yr => filingYears.add(splitYearQuarter(yr)[0]))
   tmpFilingYears = []
   filingYears.forEach(yr => tmpFilingYears.push(yr))
   
   // As Ints, sorted descending
-  filingYears = tmpFilingYears.filter(x => x).map(x => parseInt(x, 10)).sort().reverse()
+  filingYears = tmpFilingYears
+    .filter((x) => x)
+    .map((x) => parseInt(x, 10))
+    .sort()
+    .reverse()
+    .map((x) => x.toString())
+
   return filingYears
 }
+
+/**
+ * Returns the years for which Filing is open
+ * @param {Object} config Environment configuration
+ */
+export const getOpenFilingYears = (config) => getFilingYears(config, ({ adminYears: false }))
