eb3329d8b31e382210f2eb9cdbab065698b94704
Meissa Dia <meissadia@gmail.com>
2020-12-30 12:46:26 -0700

    WIP: investigation 1

--- src/filing/common/CSVContainer.jsx
+++ src/filing/common/CSVContainer.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import fetchCSV from '../actions/fetchCSV.js'
 import CSVDownload from './CSVDownload.jsx'
+import * as AccessToken from '../api/AccessToken'
+import {saveAs} from 'file-saver'
 
 export class CSVContainer extends Component {
   constructor() {
@@ -28,11 +30,35 @@ export function mapStateToProps(state, ownProps) {
 export function mapDispatchToProps(dispatch) {
   // triggered by a click on "Download edit report"
   const onDownloadClick = function(lei, filing, submissionId) {
-    return e => {
-      e.preventDefault()
-      this.setState({ isFetching: true })
-      dispatch(fetchCSV(lei, filing, submissionId)).then(() => {
-        this.setState({ isFetching: false })
+    // return e => {
+    //   e.preventDefault()
+    //   this.setState({ isFetching: true })
+    //   dispatch(fetchCSV(lei, filing, submissionId)).then(() => {
+    //     this.setState({ isFetching: false })
+    //   })
+    // }
+    return () => {
+      console.log('Filing: ', filing)
+      let url = `/v2/filing/institutions/${lei}/filings/${filing}/submissions/${submissionId}/edits/csv?format=csv`
+      let filename = `${lei}-${submissionId}-full-edit-report.csv`
+      // let a = document.createElement('a')
+      // a.href = url
+      // a.setAttribute('download', filename)
+      // a.click()
+      // a = null
+
+      console.log('Running "fetch" for ', url)
+      fetch(url, {
+        headers: {
+          "Authorization": `Bearer ${AccessToken.get()}`,
+          'Content-Type': 'text/csv'
+        }
+      }).then(res => {
+        console.log('Response completed!')
+        return res.blob()
+      }).then(blob => {
+        console.log('We have the Blob!')
+        saveAs(blob, filename)
       })
     }
   }
