b2e590c03e81d5c9444847cbc03b476dc7735826
Meissa Dia <meissadia@gmail.com>
2021-06-16 12:30:54 -0600

    fix(data-pub): [Dynamic] Update links to point to ZIP files instead of uncompressed text files

--- src/data-publication/constants/dynamic-dataset.js
+++ src/data-publication/constants/dynamic-dataset.js
@@ -1,15 +1,15 @@
 export const DYNAMIC_DATASET = {
   2020: {
-    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/dev/dynamic-data/2020/2020_lar.txt',
-    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/dev/dynamic-data/2020/2020_ts.txt',
+    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/dev/dynamic-data/2020/2020_lar.zip',
+    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/dev/dynamic-data/2020/2020_ts.zip',
   },
   2019: {
-    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2019/2019_lar.txt',
-    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2019/2019_ts.txt',
+    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2019/2019_lar.zip',
+    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2019/2019_ts.zip',
   },
   2018: {
-    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2018/2018_lar.txt',
-    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2018/2018_ts.txt',
+    lar : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2018/2018_lar.zip',
+    ts : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/2018/2018_ts.zip',
   },
   2017: {
     lar : 'https://s3.amazonaws.com/cfpb-hmda-public/prod/dynamic-data/dynamic_lar_ultimate_2017.txt',
