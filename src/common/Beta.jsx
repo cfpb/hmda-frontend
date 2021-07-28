b27d2be7659b21eed52d0d16add214b122ed5db0
meissadia <meissadia@gmail.com>
2021-07-27 20:12:33 -0600

    task(beta): Update header with cache suggestion

--- src/common/Beta.jsx
+++ src/common/Beta.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Alert from './Alert.jsx'
 
 export function isBeta(host=window.location.hostname) {
-  return host.match('beta')
+  return true || host.match('beta')
 }
 
 const Beta = props => {
@@ -15,8 +15,10 @@ const Beta = props => {
           only and may be removed at any time. You will need to file your final
           HMDA data to the live system when the filing period is open.
           <br />
-          To view the live version of all HMDA applications,{' '}
-          <a href='https://ffiec.cfpb.gov'>visit our homepage</a>. For
+          To view the live version of all HMDA applications,{" "}
+          <a href="https://ffiec.cfpb.gov">visit our homepage</a>. If you are
+          having issues uploading a file and are receiving errors, please
+          refresh the page and clear your browser’s cache. For
           questions/suggestions, contact hmdahelp@cfpb.gov.
         </p>
       </Alert>
