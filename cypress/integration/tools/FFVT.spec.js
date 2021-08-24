581542a8768ccc66ef873c5c24d28a0ce8dbbf2a
meissadia <meissadia@gmail.com>
2021-08-24 14:35:55 -0600

    task(cypress): Add FFVT test for file encoding and BOM

--- cypress/integration/tools/FFVT.spec.js
+++ cypress/integration/tools/FFVT.spec.js
@@ -6,8 +6,11 @@ const { HOST, TEST_DELAY, ENVIRONMENT } = Cypress.env()
 const openYears = getOpenFilingYears(getDefaultConfig(HOST))
 
 const CLEAN_FILE = 'FFVT-2018-clean.txt'
+const CLEAN_FILE_BOM = 'FFVT-2018-clean-w-bom.txt'
 const ERROR_FILE = 'FFVT-2018-error.txt'
 const INVALID_FILE_1 = 'FFVT-Invalid-single-error.abc'
+const INVALID_FILE_2 = 'FFVT-Invalid-multi-error.abc'
+const INVALID_FILE_ENCODING = 'FFVT-Invalid-utf16.txt'
 
 describe('FFVT', function () {
   beforeEach(() => {
@@ -35,6 +38,33 @@ describe('FFVT', function () {
         ).attachFile({
           fileContent,
           fileName: CLEAN_FILE,
+          mimeType: 'text/plain',
+        })
+      })
+
+      // Validate
+      cy.get('.alert-success').contains('Congratulations')
+      cy.wait(TEST_DELAY)
+    })
+
+    it(`Validates a clean file with BOM for ${year}`, function () {
+      cy.get({ HOST, TEST_DELAY, ENVIRONMENT }).logEnv()
+      cy.get('select').select(year)
+      cy.get(
+        'div > .UploadForm > .container-upload > .dropzone > .dropzone-content'
+      ).click()
+      cy.get('div > .UploadForm > .container-upload > .dropzone > input').click(
+        {
+          force: true,
+        }
+      )
+
+      cy.fixture(CLEAN_FILE_BOM).then((fileContent) => {
+        cy.get(
+          'div > .UploadForm > .container-upload > .dropzone > input'
+        ).attachFile({
+          fileContent,
+          fileName: CLEAN_FILE_BOM,
           mimeType: 'text/plain',
         })
       })
@@ -75,7 +105,7 @@ describe('FFVT', function () {
       cy.wait(TEST_DELAY)
     })
 
-    it('Identifies INVALID files', () => {
+    it('Identifies invalid file extensions', () => {
       cy.get('select').select(year)
       cy.get(
         'div > .UploadForm > .container-upload > .dropzone > .dropzone-content'
@@ -99,6 +129,61 @@ describe('FFVT', function () {
       // Validate
       cy.get('.alert-error .alert-heading').contains('Invalid File')
       cy.get('.alert-error .alert-text').contains('Please check your file and re-upload.').should('have.length', 1)
+      cy.wait(TEST_DELAY)
+    })
+
+    it('Identifies invalid file encoding', () => {
+      cy.get('select').select(year)
+      cy.get(
+        'div > .UploadForm > .container-upload > .dropzone > .dropzone-content'
+      ).click()
+      cy.get('div > .UploadForm > .container-upload > .dropzone > input').click(
+        {
+          force: true,
+        }
+      )
+
+      cy.fixture(INVALID_FILE_ENCODING).then((fileContent) => {
+        cy.get(
+          'div > .UploadForm > .container-upload > .dropzone > input'
+        ).attachFile({
+          fileContent,
+          fileName: INVALID_FILE_ENCODING,
+          mimeType: 'text/plain',
+        })
+      })
+
+      // Validate
+      cy.get('.alert-error .alert-heading').contains('Invalid File')
+      cy.get('.alert-error .alert-text').contains('The file you uploaded is not UTF-8 encoded. Please check your file and re-upload.').should('have.length', 1)
+      cy.wait(TEST_DELAY)
+    })
+    
+    it('Identifies multiple file validity issues', () => {
+      cy.get('select').select(year)
+      cy.get(
+        'div > .UploadForm > .container-upload > .dropzone > .dropzone-content'
+      ).click()
+      cy.get('div > .UploadForm > .container-upload > .dropzone > input').click(
+        {
+          force: true,
+        }
+      )
+
+      cy.fixture(INVALID_FILE_2).then((fileContent) => {
+        cy.get(
+          'div > .UploadForm > .container-upload > .dropzone > input'
+        ).attachFile({
+          fileContent,
+          fileName: INVALID_FILE_2,
+          mimeType: 'text/plain',
+        })
+      })
+
+      // Validate
+      cy.get('.alert-error .alert-heading').contains('Invalid File')
+      cy.get('.alert-error .alert-text').contains('The file you uploaded is not UTF-8 encoded. Please check your file and re-upload.').should('have.length', 1)
+      cy.get('.alert-error .alert-text').contains('The file you uploaded is not a text file (.txt). Please check your file and re-upload.').should('have.length', 1)
       cy.wait(TEST_DELAY)
     })
   })
