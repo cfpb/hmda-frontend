9aafac914a2dca2141ed75cf847de9be16a38a82
meissadia <meissadia@gmail.com>
2021-09-21 14:47:04 -0600

    Documentation Dev process: Create script to copy Documentation files for local preview

--- src/documentation/markdownUtils.js
+++ src/documentation/markdownUtils.js
@@ -5,7 +5,12 @@ function isBadYear(year){
 }
 
 function getMarkdownUrl(year, slug) {
-  return `https://raw.githubusercontent.com/cfpb/hmda-frontend/master/src/documentation/markdown/${year}/${slug}.md`
+  const localOrRemoteUrl =
+    window.location.hostname.indexOf("localhost") > -1
+      ? window.location.origin
+      : "https://raw.githubusercontent.com/cfpb/hmda-frontend/master/src/documentation"
+      
+  return `${localOrRemoteUrl}/markdown/${year}/${slug}.md`
 }
 
 export {
