3ec75756c8296a24c82621ebea0eae70bdb20ab3
meissadia <meissadia@gmail.com>
2021-10-18 15:19:42 -0600

    ChangeLog - Refactor tests for easier readability

--- cypress/integration/data-publication/ChangeLog.spec.js
+++ cypress/integration/data-publication/ChangeLog.spec.js
@@ -1,54 +1,68 @@
-import log from '../../../src/data-publication/ChangeLog/change-log-data.json'
+import log from "../../../src/data-publication/ChangeLog/change-log-data.json"
 const { HOST } = Cypress.env()
-const EXPECTED_SELECTED_PILLS = ['release', 'documentation', 'tools']
+const EXPECTED_SELECTED_PILLS = ["release", "documentation", "tools"]
 const entries = log.log
 
-describe('Change Log', () => {
-  describe('Filter Bar', () => {
-    it('Loads config from query string', () => {
-      cy.visit(`${HOST}/data-publication/updates?type=release&product=documentation,tools&keywords=2020,tool`)
-      
-      cy.log(">> Validate pills get set on load")
-      EXPECTED_SELECTED_PILLS.forEach(pillClass => {
+describe("Change Log", () => {
+  describe("Filter Bar", () => {
+    it("Sets Type and Product filters from URL query string", () => {
+      cy.visit(
+        `${HOST}/data-publication/updates?type=release&product=documentation,tools`
+      )
+      EXPECTED_SELECTED_PILLS.forEach((pillClass) => {
         cy.get(`#filter-bar .pill.selected.${pillClass}`)
       })
+    })
 
-      cy.log(">> Check keyword filter works")
-      cy.get('.change-row').should('have.length', 2)
-      cy.get('.result-count .body').should('contain', 'Showing ' + 1 + ' out of')
-      cy.get('.change-row').find('.highlighted').its('length').should('be.gte', 2)
+    it("Applies keyword filter from URL query string", () => {
+      cy.visit(`${HOST}/data-publication/updates?keywords=2020,tool`)
+      cy.get(".change-row").should("have.length", 2)
+      cy.get(".result-count .body").should(
+        "contain",
+        "Showing " + 1 + " out of"
+      )
+      cy.get(".change-row")
+        .find(".highlighted")
+        .its("length")
+        .should("be.gte", 2)
 
-      cy.get('.reset-filters').click()
-      cy.get('.change-row').should('have.length', entries.length + 1)
-      cy.get('.result-count').should('not.exist')
+      cy.get(".reset-filters").click()
+      cy.get(".change-row").should("have.length.gte", entries.length + 1)
+      cy.get(".result-count").should("not.exist")
+    })
 
-      cy.log(">> Changing filters should update the URL")
-      cy.get('#filter-bar').findByText('correction').click()
-      cy.url().should('contain', '?type=correction')
-      cy.get('.change-row:not(.header)').should('exist')
-      cy.get('.result-count .body').should('contain', 'Showing ' + 7 + ' out of')
+    it("Adds filters to URL", () => {
+      cy.visit(`${HOST}/data-publication/updates`)
+      cy.get("#filter-bar").findByText("correction").click()
+      cy.url().should("contain", "?type=correction")
+      cy.get(".change-row:not(.header)").should("exist")
 
-      cy.get('#filter-bar').findByText('update').click()
-      cy.get('#filter-bar').findByText('HMDA Tools').click()
-      cy.url().should('contain', '?type=correction,update&product=tools')
-      cy.get('.change-row:not(.header)').should('have.length', 5)
-      cy.get('.result-count .body').should('contain', 'Showing ' + 5 + ' out of')
-      
-      cy.log(">> Search by Keyword")
-      cy.findByLabelText('by Change Description').type('2018')
-      cy.url().should('contain', '?type=correction,update&product=tools&keywords=2018')
-      cy.get('.change-row').should('have.length', 2)
-      cy.get('.result-count .body').should('contain', 'Showing ' + 1 + ' out of')
+      cy.get("#filter-bar").findByText("update").click()
+      cy.get("#filter-bar").findByText("HMDA Tools").click()
+      cy.url().should("contain", "?type=correction,update&product=tools")
+      cy.get(".change-row:not(.header)").should("have.length", 5)
+    })
+
+    it("Filters by keyword", () => {
+      cy.visit(`${HOST}/data-publication/updates?product=tools`)
+      cy.findByLabelText("by Change Description").type(
+        "The 2017 File Format Verification Tool"
+      )
+      cy.url().should(
+        "contain",
+        "keywords=The,2017,File,Format,Verification,Tool"
+      )
+      cy.get(".change-row").should("have.length", 2)
+      cy.get(".result-count .body").should(
+        "contain",
+        "Showing " + 1 + " out of"
+      )
       cy.log(">> Highlight keywords")
-      cy.get('.change-row .highlighted').should('have.length', 1)
+      cy.get(".change-row .highlighted").should("have.length", 7)
 
       cy.log(">> Clear keyword search with button")
-      cy.get('.clear-text').click()
-      cy.get('.change-row:not(.header)').should('have.length', 5)
-      cy.get('.result-count .body').should('contain', 'Showing ' + 5 + ' out of')
-      
-      // TODO: Test that entries are sorted by date
-      // TODO: Only show entries for the past year
+      cy.get(".clear-text").click()
+      cy.get(".change-row:not(.header)").should("have.length.gte", 7)
     })
   })
 })
