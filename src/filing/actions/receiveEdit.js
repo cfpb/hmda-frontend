1463785b5e6ead196f10086df3814353f5364566
meissadia <meissadia@gmail.com>
2021-11-03 22:30:38 -0600

    [Submission] Edits - Configurable per-Edit transformations

--- src/filing/actions/receiveEdit.js
+++ src/filing/actions/receiveEdit.js
@@ -1,6 +1,58 @@
+import React from 'react'
 import * as types from '../constants'
 
+const PATTERN_1111 = /1111/
+
+/**
+ *  Restrict the displayed fields to those whose value matches a pattern 
+ */
+const filterEditFields = (rows = [], pattern = PATTERN_1111) => {
+  let fieldMatches = {}
+
+  // Identify field values that match this pattern
+  rows.forEach((r) => {
+    if (!r.fields) return
+    r.fields.forEach((f) => {
+      if (!f.value.match(pattern)) return
+      fieldMatches[f.name] = true
+    })
+  })
+
+  // Retain only those fields
+  rows.forEach((r) => {
+    if (!r.fields) return
+    r.fields = r.fields.filter((f) => {
+      if (!fieldMatches[f.name]) return null
+      if (f.value.match(pattern)) {
+        // Highlight the offending field
+        f.value = <span className='highlight'>{f.value}</span>
+      }
+      return f
+    })
+  })
+}
+
+/**
+ * Configure transformations per Edit
+ */
+const TRANSFORMERS = {
+  Q656: {
+    type: 'regex',
+    filter: filterEditFields,
+    pattern: PATTERN_1111,
+  },
+  Q657: {
+    type: 'regex',
+    filter: filterEditFields,
+    pattern: PATTERN_1111,
+  },
+}
+
 export default function receiveEdit(data) {
+  const useSpecial = TRANSFORMERS[data.edit]
+  if (useSpecial && useSpecial.type === 'regex')
+    useSpecial.filter(data.rows, useSpecial.pattern)
+
   return {
     type: types.RECEIVE_EDIT,
     edit: data.edit,
@@ -8,7 +60,7 @@ export default function receiveEdit(data) {
     pagination: {
       count: data.count,
       total: data.total,
-      _links: data._links
-    }
+      _links: data._links,
+    },
   }
 }
