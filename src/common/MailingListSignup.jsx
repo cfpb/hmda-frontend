51e663956899763756ca8f4b35ecb252ff57997d
meissadia <meissadia@gmail.com>
2021-11-17 12:21:50 -0700

    Extract cf.gov constants for easier reuse

--- src/common/MailingListSignup.jsx
+++ src/common/MailingListSignup.jsx
@@ -1,20 +1,7 @@
 import React from 'react'
+import { SUBSCRIPTION_ENDPOINT, HMDA_FILING_TOPIC_ID } from './constants/cfgov'
 import { useSubscriptionLogic } from './mailingListServices'
-import { isProd } from './configUtils'
 import './MailingListSignupForm.css'
-
-// CF.gov host for Production/Development environments
-const CFGOV_HOST = isProd(window.location.host)
-  ? 'https://www.consumerfinance.gov'
-  : `${process.env.REACT_APP_CFGOV_DEV_DOMAIN}`
-
-// CF.gov endpoint for subscribing to a topic
-const SUBSCRIPTION_ENDPOINT = `${CFGOV_HOST}/subscriptions/new/`
-
-// HMDA topic for Production/Staging
-const HMDA_FILING_TOPIC_ID = isProd(window.location.host)
-  ? 'USCFPB_139'
-  : 'USCFPB_51'
 
 // Common form heading
 const Heading = () => (
