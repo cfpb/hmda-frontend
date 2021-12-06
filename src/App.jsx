4fc61af4a2f8bcf634d1380e92970dccca82e358
meissadia <meissadia@gmail.com>
2021-12-06 14:19:57 -0700

    Use TimedGuards to derive filing period statuses

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import makeAsyncComponent from './common/makeAsyncComponent.js'
 import { useEnvironmentConfig } from './common/useEnvironmentConfig'
 import { betaLinks, defaultLinks, updateFilingLink } from './common/constants/links'
 import { AppContext } from './common/appContextHOC'
+import { deriveFilingPeriodStatus } from './deriveFilingPeriodStatus'
 
 import './app.css'
 
@@ -27,13 +28,15 @@ const HmdaHelp = makeAsyncComponent(() => import('./hmda-help'),
   'HMDA Help', 'Use HMDA Help to manage your HMDA Institutions and Publications')
 
 const App = () => {
-  const config = useEnvironmentConfig(window.location.hostname)
+  const config = deriveFilingPeriodStatus(
+    useEnvironmentConfig(window.location.hostname)
+  )
   const isFiling = !!window.location.pathname.match(/^\/filing/)
   const isHelp = !!window.location.pathname.match(/^\/hmda-help/)
 
   const showCommonHeader = !(isFiling || isHelp)
   const showFooter = !isHelp
-  const showBetaBanner = isBeta() && !isFiling 
+  const showBetaBanner = isBeta() && !isFiling
 
   const headerLinks = isBeta()
     ? betaLinks
---
+++ src/deriveFilingPeriodStatus.jsx
@@ -0,0 +1,106 @@
+const PERIODS = ['Q3', 'Q2', 'Q1', 'annual']
+
+function potentialYears(start = 2018) {
+  let current = start < 2018 ? 2018 : start
+  const output = []
+  const maxYear = new Date().getFullYear() + 1
+
+  while (current <= maxYear) {
+    output.unshift(current)
+    current++
+  }
+  return output
+}
+
+function parseTimedGuardDate(str, isDeadline = false) {
+  let [month, day, year] = str.split('/').map((s) => parseInt(s, 10))
+  month = month - 1 // Javascript months are 0 indexed
+
+  // Determine time distance from Eastern Time zone
+  let offset = new Date().getTimezoneOffset() / 60 - 5
+
+  if (isDeadline) // Check against the "end of the day"
+    return new Date(
+      year,
+      month,
+      day,
+      23 - offset, // 11:59pm ET
+      59,
+      59
+    )
+
+  // Check against the "start of the day"
+  return new Date(
+    year,
+    month,
+    day,
+    0 - offset, // 12am ET
+    0,
+    0
+  )
+}
+
+function formatLocalString(date) {
+  return date.toLocaleString('en-US', {
+    year: 'numeric',
+    day: 'numeric',
+    month: 'long'
+  })
+}
+
+// Determine 
+export function deriveFilingPeriodStatus(baseConfig) {
+  const config = JSON.parse(JSON.stringify(baseConfig))
+  const timedGuards = config.timedGuards
+  const now = Date.now()
+  const dateIsDeadline = [false, true, true]
+
+  config.filingPeriodStatus = {}
+
+  potentialYears().forEach((year) => {
+    PERIODS.forEach((period) => {
+      if (!timedGuards[year][period]) return
+
+      let periodString = `${year}`
+      if (period.includes('Q'))
+        periodString += `-${period}`
+
+      // Parse timed guards
+      const [startOfCollection, startOfLateFiling, collectionDeadline] = timedGuards[year][period]
+        .split(' - ')
+        .map((dateString, idx) => parseTimedGuardDate(dateString, dateIsDeadline[idx])
+        )
+
+      // Collect all pertinant info about the filing period
+      config.filingPeriodStatus[periodString] = {
+        startDate: formatLocalString(startOfCollection),
+        lateDate: formatLocalString(startOfLateFiling),
+        endDate: formatLocalString(collectionDeadline),
+        isVisible: true
+      }
+
+      if (period.includes('Q'))
+        config.filingPeriodStatus[periodString].isQuarterly = true
+
+      // Preview periods are considered "Open"
+      if (timedGuards.preview.includes(periodString)) {
+        config.filingPeriodStatus[periodString].isOpen = true
+        return
+      }
+
+      // Set the filing period status based on timed guards
+      if (now > collectionDeadline)
+        config.filingPeriodStatus[periodString].isClosed = true
+      else if (now > startOfLateFiling)
+        config.filingPeriodStatus[periodString].isLate = true
+      else if (now > startOfCollection)
+        config.filingPeriodStatus[periodString].isOpen = true
+      else {
+        config.filingPeriodStatus[periodString].isClosed = true
+        config.filingPeriodStatus[periodString].isVisible = false
+      }
+    })
+  })
+
+  return config
+}
