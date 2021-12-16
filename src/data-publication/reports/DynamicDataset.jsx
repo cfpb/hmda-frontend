5a67216bb5946e532954c469cfd27b338f4355d1
meissadia <meissadia@gmail.com>
2021-12-16 14:31:58 -0700

    Dynamic Datasets - Show if dataset is still being updated

--- src/data-publication/reports/DynamicDataset.jsx
+++ src/data-publication/reports/DynamicDataset.jsx
@@ -4,6 +4,9 @@ import YearSelector from '../../common/YearSelector.jsx'
 import { DYNAMIC_DATASET } from '../constants/dynamic-dataset.js'
 import { withAppContext } from '../../common/appContextHOC.jsx'
 import { S3DatasetLink } from '../../common/S3Integrations'
+import { ReactComponent as PlayButton } from '../../common/images/iconPlayButton.svg';
+import { ReactComponent as StopButton } from '../../common/images/iconStopButton.svg';
+
 import './DynamicDataset.css'
 
 const linkToDocs2017 = ({ lar_spec, ts_spec }) => [
@@ -11,34 +14,49 @@ const linkToDocs2017 = ({ lar_spec, ts_spec }) => [
   <S3DatasetLink href={ts_spec} label='Transmittal Sheet Records (TS)' />
 ]
 
-function linkToDocs(year = '2018'){
+function linkToDocs(year = '2018') {
   return [
-    <li key="0"><a href={`/documentation/${year}/public-lar-schema/`}>Public LAR Schema</a></li>,
-    <li key="1"><a href={`/documentation/${year}/lar-data-fields/`}>Public LAR Field Definitions and Values</a></li>,
-    <li key="2"><a href={`/documentation/${year}/public-ts-schema/`}>Public Transmittal Sheet Schema</a></li>,
+    <li key='0'>
+      <a href={`/documentation/${year}/public-lar-schema/`}>
+        Public LAR Schema
+      </a>
+    </li>,
+    <li key='1'>
+      <a href={`/documentation/${year}/lar-data-fields/`}>
+        Public LAR Field Definitions and Values
+      </a>
+    </li>,
+    <li key='2'>
+      <a href={`/documentation/${year}/public-ts-schema/`}>
+        Public Transmittal Sheet Schema
+      </a>
+    </li>
   ]
 }
-
 
 const DynamicDataset = props => {
   const { params, url } = props.match
   const year = params.year
-  const { dynamic, shared  } = props.config.dataPublicationYears
-  const years =  dynamic || shared
+  const { filingPeriodStatus, dataPublicationYears } = props.config
+  const { dynamic, shared } = dataPublicationYears
+  const years = dynamic || shared
   const dataForYear = DYNAMIC_DATASET[year]
+
+  const status = filingPeriodStatus[year]
 
   return (
     <div className='DynamicDataset' id='main-content'>
       <Heading
         type={1}
         headingText='Dynamic National Loan-Level Dataset'
-        paragraphText='The dynamic files contain the national HMDA datasets for
+        paragraphText={`The dynamic files contain the national HMDA datasets for
           all HMDA reporters, modified by the Bureau to protect applicant and
           borrower privacy, updated to include late submissions and
           resubmissions. The dynamic files are available to download in a pipe
-          delimited text file format. The dynamic datasets are updated on Mondays
-          with HMDA submissions received through the previous Sunday night.'
-      />
+          delimited text file format. The dynamic datasets are updated on Mondays with HMDA submissions received through the previous Sunday night.`}
+      >
+        <UpdateSchedule {...{ status, publication: 'dynamic' }} />
+      </Heading>
 
       <YearSelector year={year} url={url} years={years} />
 
@@ -65,12 +83,75 @@ const DynamicDataset = props => {
               headingText={year + ' Dynamic File Specifications'}
             />
             <ul>
-              {year === '2017' ? linkToDocs2017(dataForYear) : linkToDocs(year)}
+              {year === '2017'
+                ? makeListLink(
+                    dataForYear.lar_spec,
+                    'Loan/Application Records (LAR)'
+                  )
+                : null}
+              {year === '2017'
+                ? makeListLink(
+                    dataForYear.ts_spec,
+                    'Transmittal Sheet Records (TS)'
+                  )
+                : null}
+              {year !== '2017' ? linkToDocs(year) : null}
             </ul>
           </div>
         </div>
       )}
     </div>
+  )
+}
+
+const BaseIconStyles = {
+  className: 'icon',
+  width: '1.2em',
+  height: '1.2em',
+}
+
+const PublicationUpdateSettings = {
+  dynamic: {
+    updated: {
+      icon: <PlayButton {...BaseIconStyles} />,
+      cname: 'updated',
+      message: ({ endDate }) => (
+        <>This dataset is open for filer resubmissions through {endDate}.</>
+      )
+    },
+    notUpdated: {
+      icon: <StopButton {...BaseIconStyles} />,
+      cname: 'notUpdated',
+      message: ({ endDate }) => (
+        <>This dataset is no longer updated as of {endDate}.</>
+      )
+    }
+  }
+}
+
+const UpdateSchedule = ({ status, publication }) => {
+  if (!status) return null // User has not made a selection
+  
+  const cellStyle = { border: 0, paddingLeft: 0 }
+  const settings = PublicationUpdateSettings[publication]
+  const isUpdated = status?.isOpen || status?.isLate
+  const { icon, message, cname } = isUpdated
+    ? settings.updated
+    : settings.notUpdated
+
+  return (
+    <table id='UpdateSchedule' className='font-lead'>
+      <tbody>
+        <tr style={{paddingBottom: 0}}>
+          <td className={'icon ' + cname} style={{ ...cellStyle,  }}>
+            {icon}
+          </td>
+          <td className='message' style={cellStyle}>
+            {message(status)}
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
 }
 
