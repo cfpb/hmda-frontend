7570ba294c48cb532191d0c4ba6dc43a3d2693e6
meissadia <meissadia@gmail.com>
2022-01-28 19:49:37 -0700

    [Large Filers] Fix typos that prevented error reporting and skipped load testing

--- cypress/docker-runner.sh
+++ cypress/docker-runner.sh
@@ -3,18 +3,19 @@
 
 post_success() 
 {
-	project="[$1]"
+	project="[${environment}][$1]"
 	runtime=$(tail -n 1 output.txt | xargs echo)
-	msg="${project} ${runtime}"
-	curl -i -X POST -H 'Content-Type: application/json' -d "{\"text\": \"CYPRESS_HOST: ${CYPRESS_HOST} Complete. :1up: ${msg}\"}" $CYPRESS_WEB_HOOK
+	msg="${project} :1up: ${runtime}"
+	echo $msg
+	curl -i -X POST -H 'Content-Type: application/json' -d "{\"text\": \"${msg}\"}" $CYPRESS_WEB_HOOK
 }
 
 post_failure() 
 {
-	project="[$1]"
-	msg="${project} Testing failed!"
+	project="[${environment}][$1]"
+	msg="${project} :old-man-yells-at-cloud-simpsons: Testing failed!"
 	echo $msg
-	curl -i -X POST -H 'Content-Type: application/json' -d "{\"text\": \"CYPRESS_HOST: ${CYPRESS_HOST} Complete.\n:old-man-yells-at-cloud-simpsons: ${msg}\nrun \`kubectl logs \$(kubectl get pods -n monitoring --sort-by=.status.startTime | grep 'hmda-cypress-${CYPRESS_ENV}' | tail -n 1 | awk 'NR==1{print \$1}') -n monitoring\` to see the logs for the last run\" }"  $CYPRESS_WEB_HOOK
+	curl -i -X POST -H 'Content-Type: application/json' -d "{\"text\": \" ${msg}\nrun \`kubectl logs \$(kubectl get pods -n monitoring --sort-by=.status.startTime | grep 'hmda-cypress-${CYPRESS_ENV}' | tail -n 1 | awk 'NR==1{print \$1}') -n monitoring\` to see the logs for the last run\" }"  $CYPRESS_WEB_HOOK
 }
 
 cleanup()
@@ -22,21 +23,26 @@ cleanup()
 	rm output.txt
 }
 
+if [[ "$CYPRESS_HOST" == *"ffiec"* ]]; then
+  environment="PROD"
+elif [[ "$CYPRESS_HOST" == *"beta"* ]]; then
+  environment="BETA"
+fi
 
 # Integration tests
 yarn cypress run --spec "cypress/integration/**"> output.txt
 if  grep -q "All specs passed!" "output.txt" ; then
 	post_success 'Integration Testing'
 else
-	post_failure'Integration Testing'
+	post_failure 'Integration Testing'
 fi
 cleanup
 
 # Load tests
-yarn cypress run --spec "cypress/load-test/**" > output.txt
+yarn cypress run --spec "cypress/load-tests/**" > output.txt
 if  grep -q "All specs passed!" "output.txt" ; then
 	post_success 'Load Testing'
 else
-	post_failure'Load Testing'
+	post_failure 'Load Testing'
 fi
 cleanup
