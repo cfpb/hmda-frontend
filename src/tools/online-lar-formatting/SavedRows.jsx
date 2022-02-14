57e7c1144a61fc984b30d33f78dc60df0dff415d
Meis <meissadia@gmail.com>
2022-02-14 15:44:07 -0700

    [olarft] Add LAR search; Reorg saved/editing sections; Restyle return-top button

--- src/tools/online-lar-formatting/SavedRows.jsx
+++ src/tools/online-lar-formatting/SavedRows.jsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from 'react'
-import { getSchema, log } from './utils'
+import React, { useMemo, useState } from 'react'
+import { getSchema, log, LAR_SCHEMA } from './utils'
 import { Table } from 'react-fluid-table'
 
 const tableHeight = rows => {
@@ -9,50 +9,86 @@ const tableHeight = rows => {
 
 const Section = ({ id, title, rows, highlightSelected, setSelected }) => {
   let body
+  const [searchFilter, setSearchFilter] = useState('')
+
+  // This memoization seems to fix the major performance bottleneck
+  const injectedRows = useMemo(() => {
+    log(`Calculating row IDs: ${title}`)
+    return rows.map((x, idx) => ({
+      ...x,
+      rowId: (idx + 1).toString(),
+    }))
+  }, [rows])
+  
+
+  const filteredRows =
+    searchFilter.length && id === 'saved-lars'
+      ? injectedRows.filter(ir =>
+          LAR_SCHEMA.some(col => ir[col.fieldName]?.includes(searchFilter))
+        )
+      : injectedRows
 
   const columns = rows.length
     ? getSchema(rows[0]['Record Identifier']).map(f => ({
         key: f.fieldName,
         header: f.fieldName,
         width: Math.max(f.fieldName.length * 10, 200),
+        content: ({ row }) => {
+          const plainValue = row[f.fieldName] || null
+          if (id.match(/^ts/)) return plainValue
+          if (!!searchFilter.length && row[f.fieldName]?.includes(searchFilter))
+            return <span className='highlight-match'>{row[f.fieldName]}</span>
+
+          return plainValue
+        },
       }))
     : null
 
-  if (!columns)
-    body = <div className='no-records'>No Records Saved</div>
+  if (!columns) body = <div className='no-records'>No Records Saved</div>
   else {
     columns.unshift({ key: 'rowId', header: 'Row #', width: 100 })
 
-    // This memoization seems to fix the major performance bottle n
-    const injectedRows = useMemo(() => {
-      log(`Calculating row IDs: ${title}`)
-      return rows.map((x, idx) => ({
-        ...x,
-        rowId: (idx + 1).toString(),
-      }))}, [rows]
-    )
-
     body = (
       <Table
-        data={injectedRows}
+        data={filteredRows}
         columns={columns}
-        tableHeight={tableHeight(injectedRows) * 32}
+        tableHeight={tableHeight(filteredRows) * 32}
         minColumnWidth={200}
-        onRowClick={(e, { index }) => setSelected(injectedRows[index])}
-        rowStyle={i => highlightSelected(injectedRows[i])}
+        onRowClick={(e, { index }) => setSelected(filteredRows[index])}
+        rowStyle={i => highlightSelected(filteredRows[i])}
       />
     )
   }
 
+  const rowCount =
+    filteredRows.length !== rows.length
+      ? `(${filteredRows.length}/${rows.length})`
+      : `(${rows.length})`
+  
   return (
     <div className='section' id={id}>
       {title && (
-        <h3 className='clickable'
+        <h3
+          className='clickable'
           onClick={() =>
-            document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' })
+            document.getElementById('file-actions')?.scrollIntoView({ behavior: 'smooth' })
           }
         >
-          {title}
+          {title} {rowCount}
+          <span className='search-box'>
+            <input
+              type='text'
+              onChange={e =>
+                setSearchFilter(e.target.value.trim())
+              }
+              placeholder='Search LAR'
+              value={searchFilter}
+              hidden={id.match(/ts/) || !rows.length}
+            />
+            {!!searchFilter.length && (
+              <button onClick={() => setSearchFilter('')}>Clear Search</button>
+            )}
+          </span>
         </h3>
       )}
       {body || null}
@@ -67,19 +103,18 @@ const TSheet = ({ rows, ...props }) => (
 const LARs = ({ rows, ...props }) => (
   <Section
     {...props}
-    title={`Loan Application Register (${rows?.length || 0})`}
+    title={`Loan Application Register`}
     rows={rows}
     id='saved-lars'
   />
 )
 
-
 export const SavedRows = ({ ts, lars, selected, setSelected, deleteRow }) => {
-  
   const highlightSelected = r => {
     if (!selected || !r) return {}
-    const highlighted = selected.id === r.id &&
-    selected['Record Identifier'] === r['Record Identifier']
+    const highlighted =
+      selected.id === r.id &&
+      selected['Record Identifier'] === r['Record Identifier']
     return highlighted ? { background: 'lightblue' } : {}
   }
 
