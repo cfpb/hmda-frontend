83f0c972a2151dc5e2d7bf3c593d5b447b0b93e3
Meis <meissadia@gmail.com>
2022-04-18 11:17:00 -0600

    [Cypress] Use `unzip` instead of `tar`

--- cypress/docker-runner.sh
+++ cypress/docker-runner.sh
@@ -54,7 +54,7 @@ fi
 # Download Submission file for load testing 
 [ ! -f ./cypress/fixtures/2020-FRONTENDTESTBANK9999-MAX.txt ] \
 	&& curl https://s3.amazonaws.com/cfpb-hmda-public/prod/cypress/2020-LargeFiler.zip > ./cypress/fixtures/2020-LargeFiler.zip \
-	&& tar -xvf cypress/fixtures/2020-LargeFiler.zip -C cypress/fixtures/
+	&& unzip -o cypress/fixtures/2020-LargeFiler.zip -d cypress/fixtures
 
 # Load test
 yarn cypress run --spec "cypress/integration/load/**" > output_load.txt
