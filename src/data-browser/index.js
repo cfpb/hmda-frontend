c0dd2605dd80fc64c20f08a52fdcfce8710eb76d
Michael Remy <mremy@Michaels-MacBook-Pro.local>
2022-05-17 12:31:42 -0700

    [Graphs] Implemented a url redirect fix for an edge case

--- src/data-browser/index.js
+++ src/data-browser/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Home from './Home'
-import Geography from './datasets/Geography.jsx'
-import MapsGraphs from './maps/MapsGraphs.jsx'
-import NotFound from '../common/NotFound'
-import { withAppContext } from '../common/appContextHOC'
-import { Graphs } from './graphs'
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "./Home";
+import Geography from "./datasets/Geography.jsx";
+import MapsGraphs from "./maps/MapsGraphs.jsx";
+import NotFound from "../common/NotFound";
+import { withAppContext } from "../common/appContextHOC";
+import { Graphs } from "./graphs";
 
-const DataBrowser = props => {
-  const { publicationReleaseYear } = props.config
+const DataBrowser = (props) => {
+  const { publicationReleaseYear } = props.config;
 
   return (
     <div className="DataBrowser App">
@@ -16,34 +16,34 @@ const DataBrowser = props => {
         <Route exact path="/data-browser" component={Home} />
         <Route
           path="/data-browser/graphs"
-          render={(r_props) => <Graphs {...r_props} {...props} /> }
+          render={(r_props) => <Graphs {...r_props} {...props} />}
         />
         <Route
           path="/data-browser/data/:year?"
-          render={(r_props) => <Geography {...r_props} {...props} /> }
+          render={(r_props) => <Geography {...r_props} {...props} />}
         />
         <Route
-          path={['/data-browser/maps/:year?', '/data-browser/maps']}
-          render={r_props => {
-            const year = r_props.match.params.year
-            let { pathname, search } = r_props.location
+          path={["/data-browser/maps/:year?", "/data-browser/maps"]}
+          render={(r_props) => {
+            const year = r_props.match.params.year;
+            let { pathname, search } = r_props.location;
 
             if (!year) {
-              if (pathname.substr(-1) !== '/') pathname = pathname + '/'
+              if (pathname.substr(-1) !== "/") pathname = pathname + "/";
               return (
                 <Redirect
                   to={`${pathname}${publicationReleaseYear}${search}`}
                 />
-              )
+              );
             }
 
-            return <MapsGraphs {...r_props} {...props} />
+            return <MapsGraphs {...r_props} {...props} />;
           }}
         />
         <Route component={NotFound} />
       </Switch>
     </div>
-  )
-}
+  );
+};
 
-export default withAppContext(DataBrowser)
+export default withAppContext(DataBrowser);
