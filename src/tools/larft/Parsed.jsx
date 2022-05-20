b2914ed7f65cc39cfca51d9148c70205997c2eb3
Michael Remy <mremy@Michaels-MacBook-Pro.local>
2022-05-20 10:34:00 -0700

    [larft] Fixed two bugs - Age and Credit Score will show a input field and dropdown if conditions are met.

--- src/tools/larft/Parsed.jsx
+++ src/tools/larft/Parsed.jsx
@@ -1,56 +1,55 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react'
-import { ParsedRow } from './ParsedRow'
-import { getSchema, parseRow } from './utils'
-import { ParsedHeader } from './ParsedHeader'
-import { ParsedTable } from './ParsedTable'
-import { applyFilter, checkHighlighted } from './parsedHelpers'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { ParsedRow } from "./ParsedRow";
+import { getSchema, parseRow } from "./utils";
+import { ParsedHeader } from "./ParsedHeader";
+import { ParsedTable } from "./ParsedTable";
+import { applyFilter, checkHighlighted } from "./parsedHelpers";
 
 function useFocusOnSelectedColumn(col, setCurrCol) {
   // Bring the focused column into view
   useEffect(() => {
-    const el = document.getElementById(`${col?.fieldName}`)
-    const grandparent = el?.parentElement?.parentElement
+    const el = document.getElementById(`${col?.fieldName}`);
+    const grandparent = el?.parentElement?.parentElement;
     if (!!grandparent) {
       grandparent.scrollIntoView({
-        block: 'nearest',
-        inline: 'nearest',
-        behavior: 'auto',
-      })
+        block: "nearest",
+        inline: "nearest",
+        behavior: "auto",
+      });
       grandparent.scrollTop = 0;
-      grandparent.style = {}
+      grandparent.style = {};
     }
+  }, [col]);
 
-  }, [col])
-  
   // Provide a function that will set focus to this column
-  const focus = target => {
-    if (col?.fieldIndex !== target?.fieldIndex) setCurrCol(target)
-  }
+  const focus = (target) => {
+    if (col?.fieldIndex !== target?.fieldIndex) setCurrCol(target);
+  };
 
-  return focus
+  return focus;
 }
 
 export const Parsed = ({
-  id = 'parsed-row',
+  id = "parsed-row",
   row,
   setRow,
   currCol,
   setCurrCol,
   textActions,
 }) => {
-  const [filter, setFilter] = useState('')
-  const setFocus = useFocusOnSelectedColumn(currCol, setCurrCol)
+  const [filter, setFilter] = useState("");
+  const setFocus = useFocusOnSelectedColumn(currCol, setCurrCol);
 
-  const _onChange = e => {
-    const newRow = { ...row }
-    newRow[e.target.id] = e.target.value
-    setRow(newRow)
-  }
+  const _onChange = (e) => {
+    const newRow = { ...row };
+    newRow[e.target.id] = e.target.value;
+    setRow(newRow);
+  };
 
   const tableRows = getSchema(row)
-    .filter(x => applyFilter(x, filter))
-    .map(column => (
+    .filter((x) => applyFilter(x, filter))
+    .map((column) => (
       <ParsedRow
         key={column.fieldName}
         column={column}
@@ -59,13 +58,13 @@ export const Parsed = ({
         highlightClass={checkHighlighted(column, currCol)}
         row={parseRow(row)}
       />
-    ))
+    ));
 
   return (
-    <div className='parsed' id={id}>
+    <div className="parsed" id={id}>
       <ParsedHeader filter={filter} setFilter={setFilter} id={id} />
       {textActions}
       <ParsedTable rows={tableRows} />
     </div>
-  )
-}
+  );
+};
--- src/tools/larft/ParsedRow.jsx
+++ src/tools/larft/ParsedRow.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Accordion } from './Accordion'
-import { MoreInfo } from './MoreInfo'
-import { buildOptions, getType, toJsDateString } from './parsedHelpers'
-import { log, OR_DELIMITER } from './utils'
-import STATES from '../../data-browser/constants/states'
+import React from "react";
+import { Accordion } from "./Accordion";
+import { MoreInfo } from "./MoreInfo";
+import { buildOptions, getType, toJsDateString } from "./parsedHelpers";
+import { log, OR_DELIMITER } from "./utils";
+import STATES from "../../data-browser/constants/states";
 
 export const ParsedRow = ({
   column,
@@ -12,21 +12,20 @@ export const ParsedRow = ({
   onChange,
   onFocus,
 }) => {
-  const userInput = buildInput(column, row, onChange)
-  const moreInfo = <MoreInfo field={column} />
-  const { fieldName, fieldIndex } = column
+  const userInput = buildInput(column, row, onChange);
+  const moreInfo = <MoreInfo field={column} />;
+  const { fieldName, fieldIndex } = column;
 
   return (
     <tr
       key={fieldName}
       className={highlightClass}
-      key={fieldName}
       onClick={() => onFocus(column)}
     >
-      <td className={'fieldIndex ' + highlightClass}>
+      <td className={"fieldIndex " + highlightClass}>
         <label htmlFor={fieldName}>{fieldIndex + 1}</label>
       </td>
-      <td className={'fieldName ' + highlightClass}>
+      <td className={"fieldName " + highlightClass}>
         <label htmlFor={fieldName}>
           <Accordion
             content={moreInfo}
@@ -35,116 +34,139 @@ export const ParsedRow = ({
           />
         </label>
       </td>
-      <td className={'fieldValue ' + highlightClass}>{userInput}</td>
+      <td className={"fieldValue " + highlightClass}>{userInput}</td>
     </tr>
-  )
-}
+  );
+};
 
 const buildPlaceholder = (exs, descs) => {
-  return (exs.length ? exs : descs).join(OR_DELIMITER)
-}
+  return (exs.length ? exs : descs).join(OR_DELIMITER);
+};
 
 function buildInput(_col, _row, _changeFn) {
-  log('Building input...')
+  log("Building input...");
 
-  if (!_col) return null
-  const { examples = [], enumerations = [], descriptions = [] } = _col
-  const placeholder = buildPlaceholder(examples, descriptions)
+  if (!_col) return null;
+  const { examples = [], enumerations = [], descriptions = [] } = _col;
+  const placeholder = buildPlaceholder(examples, descriptions);
   const common = {
     id: _col.fieldName,
     key: _col.fieldName,
     name: _col.fieldName,
     onChange: _changeFn,
     style: {
-      border: '1px dotted grey',
-      width: '100%',
-      height: '100%',
-      maxHeight: '3em',
-      paddingLeft: '5px',
+      border: "1px dotted grey",
+      width: "100%",
+      height: "100%",
+      maxHeight: "3em",
+      paddingLeft: "5px",
     },
-  }
+  };
 
   // State selector
-  if (_col.fieldName?.includes('State')) {
+  if (_col.fieldName?.includes("State")) {
     return (
-      <select {...common} value={_row[_col.fieldName] || ''}>
+      <select {...common} value={_row[_col.fieldName] || ""}>
         {buildOptions({
-          enumerations: STATES.filter(state => !['NW'].includes(state.id)).map(
-            obj => ({
-              value: obj.id,
-              description: obj.name,
-            })
-          ),
+          enumerations: STATES.filter(
+            (state) => !["NW"].includes(state.id)
+          ).map((obj) => ({
+            value: obj.id,
+            description: obj.name,
+          })),
         })}
       </select>
-    )
+    );
   }
   // Field allows freeform text but also has enumerated values
   else if (
     examples.length &&
     enumerations.length &&
-    enumerations.some(e => ['NA', 'Exempt'].includes(e.value))
+    // Accepted Enumerations - List will need to be updated with new entries.
+    // Buttons or dropdowns are generated from enumerations
+    enumerations.some((e) =>
+      ["NA", "Exempt", "7777", "8888", "9999", "No co-applicant"].includes(
+        e.value
+      )
+    )
   ) {
-    const derivedInputField = _col.fieldName.includes('Date') ? (
+    const derivedInputField = _col.fieldName.includes("Date") ? (
       <input
-        className='date-input'
+        className="date-input"
         {...common}
-        type='date'
-        onChange={e => {
+        type="date"
+        onChange={(e) => {
           _changeFn({
             target: {
               id: e.target.id,
-              value: e.target.value?.replaceAll('-', ''),
+              value: e.target.value?.replaceAll("-", ""),
             },
-          })
+          });
         }}
-        value={toJsDateString(_row[_col.fieldName]?.toString() || '')}
+        value={toJsDateString(_row[_col.fieldName]?.toString() || "")}
       />
     ) : (
       <input
-        className='text-input'
+        className="text-input"
         {...common}
         type={getType(_col)}
-        value={_row[_col.fieldName]?.toString() || ''}
+        value={_row[_col.fieldName]?.toString() || ""}
         placeholder={placeholder}
       />
-    )
+    );
     return (
-      <div className='enum-entry'>
+      <div className="enum-entry">
         {derivedInputField}
-        <span className='enums'>
-          {enumerations.map((e, idx) => (
-            <button
-              key={`${e.value}-${idx}`}
-              className={
-                e.value === _row[_col.fieldName] ? 'enum selected' : 'enum'
-              }
-              onClick={() =>
-                _changeFn({ target: { id: _col.fieldName, value: e.value } })
-              }
-            >
-              {e.value}
-            </button>
-          ))}
+        <span className="enums">
+          {/* Generate a dropdown with enumeration options if enumerations length is 3 or larger */}
+          {enumerations.length >= 3 ? (
+            <select {...common} value={_row[_col.fieldName] || ""}>
+              {enumerations.map((e, idx) => {
+                return (
+                  <option key={`${e.value}-${idx}`} value={e.value}>
+                    {e.description}
+                  </option>
+                );
+              })}
+            </select>
+          ) : (
+            enumerations.map((e, idx) => {
+              return (
+                <button
+                  key={`${e.value}-${idx}`}
+                  className={
+                    e.value === _row[_col.fieldName] ? "enum selected" : "enum"
+                  }
+                  onClick={() =>
+                    _changeFn({
+                      target: { id: _col.fieldName, value: e.value },
+                    })
+                  }
+                >
+                  {e.description !== e.value ? e.description : e.value}
+                </button>
+              );
+            })
+          )}
         </span>
       </div>
-    )
+    );
   } else if (enumerations.length) {
     // Enumerations only
     return (
-      <select {...common} value={_row[_col.fieldName] || ''}>
+      <select {...common} value={_row[_col.fieldName] || ""}>
         {buildOptions(_col)}
       </select>
-    )
+    );
   } else {
     // Examples only
     return (
       <input
         {...common}
         type={getType(_col)}
-        value={_row[_col.fieldName] || ''}
+        value={_row[_col.fieldName] || ""}
         placeholder={placeholder}
       />
-    )
+    );
   }
 }
