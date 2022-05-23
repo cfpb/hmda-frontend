c03cc79bc8143045e52d2520731bb8fe80c7e78a
Michael Remy <mremy@Michaels-MacBook-Pro.local>
2022-05-23 09:45:10 -0700

    [Graphs] Graph Dropdown now contains categories

--- src/data-browser/graphs/utils/mockData.js
+++ src/data-browser/graphs/utils/mockData.js
@@ -1,36 +1,38 @@
-import { produce } from 'immer'
-import { yearQuarters } from '../config'
-import { graphOptions } from '../graphOptions'
+import { produce } from "immer";
+import { yearQuarters } from "../config";
+import { graphOptions, graphOptionsCategory } from "../graphOptions";
 
 // Drop-down options
 export const availableGraphs = graphOptions.map((g) => ({
   value: g.id,
   label: g.title,
+  category: g.category,
 }));
 
 export const periodOpts = yearQuarters.map((yq) => ({ value: yq, label: yq }));
 
-
 // Graph data
-export const graphA_data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-export const randomize = (num) => Math.round(num * Math.random() * 10)
+export const graphA_data1 = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+];
+export const randomize = (num) => Math.round(num * Math.random() * 10);
 
 export const mockFetchedData = (selected, data, setData) =>
-setTimeout(() => {
-  const nextState = produce(data, draft => {
-    draft[selected.id] = [
-      {
-        name: 'Closed-End',
-        data: graphA_data1.map(randomize),
-        yAxis: 0,
-      },
-      {
-        name: 'Open-End',
-        data: graphA_data1.map(randomize),
-        yAxis: 0,
-      },
-    ]
-  })
+  setTimeout(() => {
+    const nextState = produce(data, (draft) => {
+      draft[selected.id] = [
+        {
+          name: "Closed-End",
+          data: graphA_data1.map(randomize),
+          yAxis: 0,
+        },
+        {
+          name: "Open-End",
+          data: graphA_data1.map(randomize),
+          yAxis: 0,
+        },
+      ];
+    });
 
-  setData(nextState)
-}, 1000)
+    setData(nextState);
+  }, 1000);
