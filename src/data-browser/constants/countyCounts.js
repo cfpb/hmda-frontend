eca2dd60832bb75e744e9e80748d4dd4003a87bc
Meis <meissadia@gmail.com>
2022-05-24 17:16:19 -0600

    [DB] Update record counts by geography to use Integer values

--- src/data-browser/constants/countyCounts.js
+++ src/data-browser/constants/countyCounts.js
@@ -2,10 +2,12 @@ import counts2017 from './recordCountsByGeography/countyCounts2017.json'
 import counts2018 from './recordCountsByGeography/countyCounts2018.json'
 import counts2019 from './recordCountsByGeography/countyCounts2019.json'
 import counts2020 from './recordCountsByGeography/countyCounts2020.json'
+import counts2021 from './recordCountsByGeography/countyCounts2021.json'
 
 export default {
   2017: counts2017,
   2018: counts2018,
   2019: counts2019,
   2020: counts2020,
+  2021: counts2021,
 }
--- src/data-browser/constants/stateCounts.js
+++ src/data-browser/constants/stateCounts.js
@@ -2,10 +2,12 @@ import counts2017 from './recordCountsByGeography/stateCounts2017.json'
 import counts2018 from './recordCountsByGeography/stateCounts2018.json'
 import counts2019 from './recordCountsByGeography/stateCounts2019.json'
 import counts2020 from './recordCountsByGeography/stateCounts2020.json'
+import counts2021 from './recordCountsByGeography/stateCounts2021.json'
 
 export default {
   2017: counts2017,
   2018: counts2018,
   2019: counts2019,
   2020: counts2020,
+  2021: counts2021,
 }
