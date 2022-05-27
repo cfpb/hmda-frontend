158bb6d9d29ad5e1dc5009055c3163ee21d3df4e
Meis <meissadia@gmail.com>
2022-05-26 19:00:12 -0600

    [DB] Update 2021 msamd Counts

--- src/data-browser/constants/msamdCounts.js
+++ src/data-browser/constants/msamdCounts.js
@@ -2,10 +2,12 @@ import counts2017 from './recordCountsByGeography/msamdCounts2017.json'
 import counts2018 from './recordCountsByGeography/msamdCounts2018.json'
 import counts2019 from './recordCountsByGeography/msamdCounts2019.json'
 import counts2020 from './recordCountsByGeography/msamdCounts2020.json'
+import counts2021 from './recordCountsByGeography/msamdCounts2021.json'
 
 export default {
   2017: counts2017,
   2018: counts2018,
   2019: counts2019,
   2020: counts2020,
+  2021: counts2021,
 }
