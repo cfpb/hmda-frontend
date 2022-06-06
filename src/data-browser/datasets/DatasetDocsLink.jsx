731c26e2833cfd41d36ade032593e0daebf130b7
Meis <meissadia@gmail.com>
2022-06-06 12:40:02 -0600

    [Data Browser] DatasetDocsLink - Add one/three year links

--- src/data-browser/datasets/DatasetDocsLink.jsx
+++ src/data-browser/datasets/DatasetDocsLink.jsx
@@ -1,45 +1,34 @@
 import React from "react";
 
+const determineDataset = (year) => {
+  let ThreeYear = [2018, 2017];
+  let OneYear = [2019];
+
+  // Link will be created after 'Three Year' doc is created
+  if (ThreeYear.includes(year))
+    return {
+      name: "Three Year",
+      link: `https://ffiec.cfpb.gov/data-publication/three-year-national-loan-level-dataset/${year}`,
+    };
+  
+  // Link will be created after 'One Year' doc is created
+  if (OneYear.includes(year))
+    return {
+      name: "One Year",
+      link: `https://ffiec.cfpb.gov/data-publication/one-year-national-loan-level-dataset/${year}`,
+    };
+  
+  return {
+    name: 'Snapshot',
+    link: `https://ffiec.cfpb.gov/data-publication/snapshot-national-loan-level-dataset/${year}`,
+  }
+};
+
 /**
  * Component that takes in a 'year' and generates Snapshot, One Year or Three Year direct links and includes link to 'Data Browser - FAQ'
  * @param {String} year string that contains the year (i.e) 2018, 2019, 2020 etc...
  */
-
 const DatasetDocsLink = ({ year }) => {
-  /* 
-  
-  Links to specfic datasets
-
-  Snapshot: https://ffiec.cfpb.gov/data-publication/snapshot-national-loan-level-dataset/2020
-  Three Year: Doc to be created
-  One Year: Doc to be created
-
-  */
-
-  const determineDataset = (year) => {
-    let ThreeYear = [2018, 2017];
-    let OneYear = [2019];
-    let Snapshot = [2020, 2021];
-
-    if (ThreeYear.includes(year))
-      return {
-        name: "Three Year",
-        link: "",
-      };
-    // Link will be created after 'Three Year' doc is created
-    else if (OneYear.includes(year))
-      return {
-        name: "One Year",
-        link: "",
-      };
-    // Link will be created after 'One Year' doc is created
-    else if (Snapshot.includes(year))
-      return {
-        name: "Snapshot",
-        link: `https://ffiec.cfpb.gov/data-publication/snapshot-national-loan-level-dataset/${year}`,
-      };
-  };
-
   let dataset = determineDataset(parseInt(year));
 
   return (
@@ -53,7 +42,7 @@ const DatasetDocsLink = ({ year }) => {
         <a
           target="_blank"
           rel="noopener noreferrer"
-          href="/documentation/2022/static-dataset-faq" // Link will be created after FAQ page has been created
+          href={`/documentation/${year}/static-dataset-faq`} // Link will be created after FAQ page has been created
         >
           Dataset - FAQ.
         </a>
