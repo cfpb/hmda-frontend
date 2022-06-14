78e586fb277da42790411fd7df717c331dc842f3
Meis <meissadia@gmail.com>
2022-06-13 18:59:16 -0600

    [Data Pub] Refactor landing page with DatasetGroup component

--- src/data-publication/Home.jsx
+++ src/data-publication/Home.jsx
@@ -16,6 +16,46 @@ const MoreInfo = ({ label, value }) => {
   )
 }
 
+const MoreInfoList = ({ infoPoints, publication }) => {
+  const infoList = []
+  const lastInfoPointIdx = infoPoints.length
+
+  infoPoints.forEach((ip, idx) => {
+    if (!publication[ip.valueKey]) return // No info point value to render
+
+    infoList.push(
+      <MoreInfo
+        label={ip.label}
+        value={publication[ip.valueKey]}
+        key={ip.label}
+      />
+    )
+
+    if (idx + 1 < lastInfoPointIdx) {
+      infoList.push(<br key={ip.label + '-spacer'} />)
+    }
+  })
+
+  return infoList
+}
+
+const DatasetGroup = ({ name, heading, publications, infoPoints, year }) => {
+  return (
+    <div className={name}>
+      <Heading type={3} headingText={heading} />
+      <div className='card-container'>
+        {publications
+          .map((pub) => (
+            <div className='card' key={`${year}-${pub.headingText}`}>
+              <Heading type={4} {...pub} />
+              <MoreInfoList infoPoints={infoPoints} publication={pub} />
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
+
 class Home extends Component {
   render() {
     const { year } = this.props.match.params
@@ -24,18 +64,18 @@ class Home extends Component {
     if (shared.indexOf(year) === -1) return <NotFound />
 
     return (
-      <div className="home">
-        <div className="intro">
-          <Heading type={1} headingText="HMDA Data Publication">
-            <p className="lead">
+      <div className='home'>
+        <div className='intro'>
+          <Heading type={1} headingText='HMDA Data Publication'>
+            <p className='lead'>
               The HMDA data and reports are the most comprehensive publicly
               available information on mortgage market activity. The data and
               reports can be used along with the{' '}
-              <a href="https://www.ffiec.gov/censusproducts.htm">Census</a>{' '}
+              <a href='https://www.ffiec.gov/censusproducts.htm'>Census</a>{' '}
               demographic information for data analysis purposes. Available
               below are the data and reports for HMDA data collected in or after
               2017. For HMDA data and reports for prior years, visit{' '}
-              <a href="https://www.ffiec.gov/hmda/hmdaproducts.htm">
+              <a href='https://www.ffiec.gov/hmda/hmdaproducts.htm'>
                 https://www.ffiec.gov/hmda/hmdaproducts.htm
               </a>
               .
@@ -50,38 +90,29 @@ class Home extends Component {
         <YearSelector year={year} years={shared} url='/data-publication' />
 
         <div className='dataset-container'>
-          <div className='dynamic'>
-            <Heading type={3} headingText='Dynamic Datasets' />
-            <div className='card-container'>
-              {publicationsByYear[year]
-                .filter(p => p.group == 'dynamic')
-                .map((pub, idx) => (
-                  <div className='card' key={`${year}-${pub.headingText}`}>
-                    <Heading type={4} {...pub} />
-                    <MoreInfo
-                      label='Update Frequency'
-                      value={pub.updateFrequency}
-                    />
-                  </div>
-                ))}
-            </div>
-          </div>
-          <div className='static'>
-            <Heading type={3} headingText='Static Datasets' />
-            <div className='card-container'>
-              {publicationsByYear[year]
-                .filter(p => p.group !== 'dynamic')
-                .map((pub, idx) => (
-                  <div className='card' key={`${year}-${pub.headingText}`}>
-                    <Heading type={4} {...pub} />
-                    <MoreInfo
-                      label='Update Frequency'
-                      value={pub.updateFrequency}
-                    />
-                  </div>
-                ))}
-            </div>
-          </div>
+          <DatasetGroup
+            year={year}
+            name='dynamic'
+            heading='Dynamic Datasets'
+            publications={publicationsByYear[year].filter(
+              p => p.group == 'dynamic'
+            )}
+            infoPoints={[
+              { label: 'Update Frequency', valueKey: 'updateFrequency' },
+            ]}
+          />
+          <DatasetGroup
+            year={year}
+            name='static'
+            heading='Static Datasets'
+            publications={publicationsByYear[year].filter(
+              p => p.group !== 'dynamic'
+            )}
+            infoPoints={[
+              { label: 'Data Freeze Date', valueKey: 'freezeDate' },
+              { label: 'Update Frequency', valueKey: 'updateFrequency' },
+            ]}
+          />
         </div>
       </div>
     )
