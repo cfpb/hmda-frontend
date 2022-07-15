55145740df3b2be79d818caa9c11cbd4d8406262
Meis <meissadia@gmail.com>
2022-07-15 15:31:02 -0600

    [Graphs] Refactor GraphHeader

--- src/data-browser/graphs/quarterly/GraphsHeader.jsx
+++ src/data-browser/graphs/quarterly/GraphsHeader.jsx
@@ -1,23 +1,24 @@
-// Loading: comes from index.jsx and it contains the length of available graph dropdowns
-// If loading length is less then 0 it will display a placeholder message until data has been fetched
-export const GraphsHeader = ({ loading, overview }) => (
-  <>
+import { DocumentationLink } from '../../../common/DocumentationLink'
+import { ContactHmdaHelp } from '../../ContactHmdaHelp'
+
+const DefaultOverview =
+  'The following graphs present data for the financial institutions reporting HMDA quarterly data.'
+
+const ExtrapolationWarning =
+  'Though the graphs provide some insight into trends for these institutions, they should not be taken to represent the behavior of all mortgage lenders during the relevant period.'
+
+export const GraphsHeader = ({ overview }) => (
+  <header className='heading'>
     <h1>HMDA Quarterly Graphs</h1>
-    {loading > 0 ? (
-      <div>
-        <p>{overview}</p>
-        <p>
-          Though the graphs provide some insight into trends for these
-          institutions, they should not be taken to represent the behavior of
-          all mortgage lenders during the relevant period.
-        </p>
-        <p><b>Use the menu below to select a graph.</b></p>
-      </div>
-    ) : (
-      <p>
-        The following graphs present data for the financial institutions
-        reporting HMDA quarterly data.
-      </p>
-    )}
-  </>
-);
+    <p className='lead'>
+      {overview || DefaultOverview} {ExtrapolationWarning}
+    </p>
+    <p className='lead'>
+      For more information please review the{' '}
+      <DocumentationLink id='quarterly-graphs-faq'>
+        HMDA Quarterly Graphs FAQ
+      </DocumentationLink>
+      . <ContactHmdaHelp subject='Quarterly Graphs' />
+    </p>
+  </header>
+)
