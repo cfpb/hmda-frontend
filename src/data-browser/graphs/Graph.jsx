e7a9574fe55ef7b2a1d242c9f6f5f9bdaf8eac2e
Meis <meissadia@gmail.com>
2022-07-18 10:12:12 -0600

    [Graphs] Refactor management of loading state into custom hook; Refactor Graph.onLoad(); Refactor drop-down category label formatter

--- src/data-browser/graphs/Graph.jsx
+++ src/data-browser/graphs/Graph.jsx
@@ -1,39 +1,32 @@
-import { useEffect, useRef } from "react";
-import Highcharts from "highcharts";
-import HighchartsReact from "highcharts-react-official";
-import HighchartsExport from "highcharts/modules/exporting";
-import HighchartsExportData from "highcharts/modules/export-data";
+import { useCallback, useRef } from 'react'
+import Highcharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official'
+import HighchartsExport from 'highcharts/modules/exporting'
+import HighchartsExportData from 'highcharts/modules/export-data'
+import useGraphLoading from './useGraphLoading'
+import { useQuery } from './utils/utils'
+import { hideUnselectedLines } from './utils/graphHelpers'
 
-HighchartsExport(Highcharts); // Enable export to image
-HighchartsExportData(Highcharts); // Enable export of underlying data
+HighchartsExport(Highcharts) // Enable export to image
+HighchartsExportData(Highcharts) // Enable export of underlying data
 
-// Use square symbols in the Legend
-Highcharts.seriesTypes.line.prototype.drawLegendSymbol =
-  Highcharts.seriesTypes.area.prototype.drawLegendSymbol;
+export const Graph = ({ options, loading }) => {
+  const chartRef = useRef()
+  const query = useQuery()
 
-export const Graph = ({ options, loading, onLoad }) => {
-  let chartRef = useRef();
+  const onLoad = useCallback(
+    chartRef => {
+      // Hide series based on URL query parameters
+      hideUnselectedLines(chartRef, query)
+    },
+    [query]
+  )
 
-  useEffect(() => {
-    let chart = chartRef.current.chart;
-    if (!chart) return;
-
-    chart.viewData(); // Update data table when data changes/period selector is used
-
-    // Parent component is fetching data
-    if (loading) {
-      chart.tooltip.hide(); // Force tooltip to hide when changing graph selection
-      chart.showLoading(); // Display Loading screen
-      // Hide the data table, which is likely showing previous selection's data
-      if (chart.dataTableDiv) chart.dataTableDiv.style.display = "none";
-    } else {
-      chart.hideLoading();
-    }
-  }, [loading, options]);
+  useGraphLoading(chartRef, loading, options)
 
   return (
-    <div className="graph-wrapper">
-      <div className="export-charts">
+    <div className='graph-wrapper'>
+      <div className='export-charts'>
         <HighchartsReact
           highcharts={Highcharts}
           options={options}
@@ -42,5 +35,5 @@ export const Graph = ({ options, loading, onLoad }) => {
         />
       </div>
     </div>
-  );
-};
+  )
+}
