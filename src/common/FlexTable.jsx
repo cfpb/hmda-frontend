83e3528e1520707944af1c188345f0fc590a93c4
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2022-07-20 12:21:20 -0400

    ui tweaks

--- src/common/FlexTable.jsx
+++ src/common/FlexTable.jsx
@@ -1,37 +1,30 @@
 import './FlexTable.css';
 import Tooltip from './Tooltip';
-import { ReactComponent as InfoIcon } from '../filing/images/info.svg'
 
 const FlexTable = props => {
   const {data, columns} = props;
   const col = columns || 3;
   const flexWidth = Math.floor(1 / col * 100) - 1;
 
-  return (data &&
+  return data ?
     <div className="flex-table">
       {data.map(item =>
         <div key={item.key} className="flex-item" style={{flex: `0 1 ${flexWidth}%`}}>
           <span>
-            {item.context.data.length > 0 && <Tooltip id={item.key} className="mytooltip">
+            {item.context.data.length > 0 && <Tooltip id={item.key} effect="solid">
               <ContextTable {...item.context} />
             </Tooltip>}
-            <span>
-              {item.value}
-              {item.context.data.length > 0 && <div className='infoIcon'>
-                <a
-                  data-tip
-                  data-for={item.key}
-                  href='#'
-                  onClick={e => e.preventDefault()}
-                >
-                  <InfoIcon />
-                </a>
-              </div>}
-            </span>
+            {item.context.data.length > 0 ?
+              <span
+                data-tip={item.value}
+                data-for={item.key}
+                className="has-context">
+                  {item.value}
+              </span> :
+              <span className="no-context">{item.value}</span>}
           </span>
         </div>)}
-    </div>) ||
-    <></>;
+    </div> : <></>;
 };
 
 const ContextTable = props => {
@@ -39,12 +32,16 @@ const ContextTable = props => {
   
   return (
     <table className="context-table">
-      <tr>
-        {keys.map(key => <th key={key} className="capitalize">{key}</th>)}
-      </tr>
-      {data.map(value => <tr key={value[keys[0]]}>
-        {keys.map(key => <td key={value[key]}>{value[key]}</td>)}
-      </tr>)}
+      <thead>
+        <tr>
+          {keys.map(key => <th key={key} className="capitalize">{key}</th>)}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(value => <tr key={value[keys[0]]}>
+          {keys.map(key => <td key={value[key]}>{value[key]}</td>)}
+        </tr>)}
+      </tbody>
     </table>
   )
 };
