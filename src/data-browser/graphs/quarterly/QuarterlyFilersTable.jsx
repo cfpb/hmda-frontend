1e8947a8f0b353d781735311d4277a36c9a3a591
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2022-07-20 12:21:20 -0400

    ui tweaks

--- src/data-browser/graphs/quarterly/QuarterlyFilersTable.jsx
+++ src/data-browser/graphs/quarterly/QuarterlyFilersTable.jsx
@@ -1,35 +1,25 @@
-import { useEffect, useState } from "react";
-import FlexTable from "../../../common/FlexTable";
-import { QuarterlyFilerInstitutionApiUrl } from "../constants";
-import "./QuarterlyFilersTable.css";
+import { useState } from 'react';
+import FlexTable from '../../../common/FlexTable';
+import LoadingIcon from '../../../common/LoadingIcon';
+import './QuarterlyFilersTable.css';
+import useInstitutionsData from './useInstitutionsData';
 
 const QuarterlyFilersTable = props => {
-  const [data, setData] = useState([]);
   const [collapsed, setCollapsed] = useState(false);
   const year = new Date().getFullYear();
 
-  useEffect(() => {
-    fetch(`${QuarterlyFilerInstitutionApiUrl}/${year}/lars/past/3`)
-      .then(response => response.json())
-      .then(d => {
-        setData(
-          d.sort((a, b) => {
-            if (a.name > b.name) return 1;
-            if (a.name < b.name) return -1;
-            return 0;
-          }).map(v => ({key: v.lei, value: v.name, context: {keys: ['year', 'count'], data: v.larCounts}}))
-        );
-      })
-      .catch(err => console.error('quarterly institution api failed', err))
-  }, []);
+  const data = useInstitutionsData(year);
 
-  const style = collapsed ? {maxHeight: 0, overflow: 'hidden'} : {};
+  const containerClasses = ['table-container'];
+  if (collapsed) {
+    containerClasses.push('collapsed');
+  }
 
   return (
     <div className="quarterly-filers-table">
-      <h2 style={{userSelect: 'none'}}>{year} Quarterly Filers <span className="expander" onClick={() => setCollapsed(!collapsed)}>{collapsed ? '+' : '-'}</span></h2>
-      <div className="table-container" style={style}>
-        <FlexTable data={data} />
+      <h2 className="table-heading" onClick={() => setCollapsed(!collapsed)}>{year} Quarterly Filers <span className="expand-collapse">{collapsed ? '\u25bc' : '\u25b2'}</span></h2>
+      <div className={containerClasses.join(' ')}>
+        {data.length ? <FlexTable data={data} /> : <LoadingIcon />}
       </div>
     </div>
   );
