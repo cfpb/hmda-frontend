59e6b6c8f2a6495c2a2f29e6bfb2f012ba1607cd
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2022-07-27 13:17:26 -0400

    add more fields

--- src/data-browser/graphs/quarterly/QuarterlyFilersTable.jsx
+++ src/data-browser/graphs/quarterly/QuarterlyFilersTable.jsx
@@ -9,43 +9,77 @@ const QuarterlyFilersTable = props => {
   const dispatch = useDispatch();
   const [year, past] = [new Date().getFullYear(), 3];
   const { data, loading } = useSelector(state => state.institutions);
+  const pastYears = [...Array(past).keys()].map(i => `${year - i - 1}`);
 
   useEffect(() => {
-    if (!data.length) {
+    if (!data) {
       dispatch(institutions.fetchInstitutionLars({ year, past }));
     }
   }, [dispatch, data]);
 
   let content = <LoadingIcon />;
 
-  const columns = useMemo(() => {
-    const countsColumns = [...Array(past).keys()].map(i => {
-      const accessorKey = `${year - i - 1}`
+  const tableColumns = useMemo(() => {
+    const countsColumns = pastYears.map(accessorKey => {
       return {
         header: `${accessorKey} Count`,
         accessorKey,
         sortingFn: 'alphanumeric',
       };
-    })
+    });
+
     return [{
       header: 'Institution',
       accessorKey: 'name',
+      sortingFn: 'text',
+    }, {
+      header: 'LEI',
+      accessorKey: 'lei',
+      enableSorting: false,
+    }, {
+      header: 'Agency',
+      accessorKey: 'agency',
       sortingFn: 'text',
     }, ...countsColumns];
   });
 
   if (loading === 'succeeded') {
-    const formattedData = data.map(({ value: name, context: {data: larCounts} }) => {
+    const tableData = data.quarterly.map(({ name, lei, agency, larCounts }) => {
       let counts = {};
-      larCounts.forEach(lar => {
-        counts[lar.year] = lar.count;
+      larCounts.forEach(ts => {
+        counts[ts.year] = ts.count;
       });
       return {
         name,
+        lei,
+        agency,
         ...counts
       }
     });
-    content = <SimpleSortTable columns={columns} data={formattedData} />;
+
+    let quarterlySums = {};
+    pastYears.forEach(yr => {
+      quarterlySums[yr] = data.quarterly.reduce((prev, curr) => {
+        const tsLar = curr.larCounts.find(ts => ts.year === yr);
+        return tsLar ? prev + tsLar.count : prev;
+      }, 0);
+    });
+
+    const customFooter = <>
+      <tr>
+        <th colSpan="3" style={{ textAlign: 'right' }}>Total of Quarterly Filers</th>
+        {pastYears.map(yr => {
+          return <td key={yr}>{quarterlySums[yr].toLocaleString()}</td>
+        })}
+      </tr>
+      <tr>
+        <th colSpan="3" style={{ textAlign: 'right' }}>Total of All Filers</th>
+        {pastYears.map(yr => {
+          return <td key={yr}>{(data.yearly.find(yearlyCount => yearlyCount.year === yr) || { count: 0 }).count.toLocaleString()}</td>
+        })}
+      </tr>
+    </>;
+    content = <SimpleSortTable columns={tableColumns} data={tableData} customFooter={customFooter} initialSort={[{id: 'name', desc: false,}]} />;
   }
 
   return (
