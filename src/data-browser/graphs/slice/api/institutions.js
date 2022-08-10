e4dd6f685e20b08ec19788d6b38c8bd7e33eeb02
Meis <meissadia@gmail.com>
2022-08-10 15:10:16 -0600

    [Graphs] Prevent crash when qfiler endpoint is unreachable

--- src/data-browser/graphs/slice/api/institutions.js
+++ src/data-browser/graphs/slice/api/institutions.js
@@ -2,4 +2,3 @@ import { QuarterlyFilerInstitutionApiUrl } from '../../constants';
 
 export const fetchQuarterliesWithLars = (year, past) => fetch(`${QuarterlyFilerInstitutionApiUrl}/${year}/lars/past/${past}`)
   .then(response => response.json())
-  .catch(err => console.error('quarterly institution api failed', err));
