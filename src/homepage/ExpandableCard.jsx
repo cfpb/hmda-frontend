c03175b1705170c03aa6ec8f8348b9c2a4d1adca
michaeldremy <michaeldremy@gmail.com>
2022-08-24 13:46:11 -0700

    [Homepage] Updated verbage and language from feedback. Also added NEW indiciator to HMDA Updates and Notes header.

--- src/homepage/ExpandableCard.jsx
+++ src/homepage/ExpandableCard.jsx
@@ -1,21 +1,26 @@
-import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { useLocalStorage } from '../common/useLocalStorage'
-import './ExpandableCard.css'
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+import { useLocalStorage } from "../common/useLocalStorage"
+import "./ExpandableCard.css"
+import NewIndicator from "./NewIndicator"
 
 export const ExpandableCard = ({
   id, // Used to track the user's last expanded/compressed status, per card, via LocalStorage
-  title = 'Card Title',
-  classname = '',
-  description = 'Description of what the information this card links to.',
+  title = "Card Title",
+  classname = "",
+  description = "Description of what the information this card links to.",
   destination, // Target endpoint for card's title link to (ex. /documentation)
   isHidden = false, // true = Do not display Card
   expandedByDefault = true, // true = child content is displayed on load
   disableExpansion = false, // true = Allow the Card to be used without expandable content
+  addNewFeatureIndicator = false, // false = Doesn't show NewIndicator component around header
   children,
 }) => {
   const [showMore, setShowMore] = useState(expandedByDefault)
-  const [storedVisibility, setStoredVisibility] = useLocalStorage(id, expandedByDefault)
+  const [storedVisibility, setStoredVisibility] = useLocalStorage(
+    id,
+    expandedByDefault
+  )
 
   const toggleShowMore = () => {
     setStoredVisibility(!showMore)
@@ -28,17 +33,23 @@ export const ExpandableCard = ({
   if (isHidden) return null
 
   /* Make the Card's header a link, if we have a destination */
-  const cardHeading = destination ? <Link to={destination}>{title}</Link> : title
-  
-  
+  const cardHeading = destination ? (
+    <Link to={destination}>
+      {title}
+      {addNewFeatureIndicator ? <NewIndicator /> : ""}
+    </Link>
+  ) : (
+    title
+  )
+
   let expandedContent = children
-  
+
   let showMoreButton = (
-    <button className='show-more' onClick={toggleShowMore}>
-      Show {showMore ? 'Less' : 'More...'}
+    <button className="show-more" onClick={toggleShowMore}>
+      Show {showMore ? "Less" : "More..."}
     </button>
   )
-  
+
   /* Hide the 'Show More' button as well as any child content */
   if (disableExpansion) {
     expandedContent = null
@@ -46,7 +57,7 @@ export const ExpandableCard = ({
   }
 
   return (
-    <header className={'expandable-card ' + classname}>
+    <header className={"expandable-card " + classname}>
       <h3>{cardHeading}</h3>
       <p>
         {description}
