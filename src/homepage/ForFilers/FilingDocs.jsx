eab81fe96ee23fbcfce2b4175a95f108bd5cd651
michaeldremy <michaeldremy@gmail.com>
2022-08-24 08:26:47 -0700

    [Homepage] Added external link to HMDA Platform API under the Filing Documentation section.

--- src/homepage/ForFilers/FilingDocs.jsx
+++ src/homepage/ForFilers/FilingDocs.jsx
@@ -1,36 +1,41 @@
-import { Link } from 'react-router-dom'
-import { ExternalLink } from '../../common/ExternalLink'
-import { ExpandableCard } from '../ExpandableCard'
+import { Link } from "react-router-dom"
+import { ExternalLink } from "../../common/ExternalLink"
+import { ExpandableCard } from "../ExpandableCard"
 
 export const FilingDocs = () => (
   <ExpandableCard
-    id='home-expand-filing-docs'
-    title='Filing Documentation'
-    description='Answers to common questions about the process of filing HMDA data.'
-    destination='/documentation'
+    id="home-expand-filing-docs"
+    title="Filing Documentation"
+    description="Answers to common questions about the process of filing HMDA data."
+    destination="/documentation"
   >
     <ul>
       <li>
-        <Link to='documentation/2022/filing-faq/'>HMDA Filing FAQ</Link>
+        <Link to="documentation/2022/filing-faq/">HMDA Filing FAQ</Link>
       </li>
       <li>
-        <Link to='/documentation/2022/identifiers-faq/'>
+        <Link to="/documentation/2022/identifiers-faq/">
           HMDA Institution Identifiers FAQ
         </Link>
       </li>
       <li>
-        <Link to='/documentation/2022/data-collection-timelines/'>
+        <Link to="/documentation/2022/data-collection-timelines/">
           Annual HMDA Data Collection Timelines
         </Link>
       </li>
       <li>
-        <Link to='/documentation/2022/quarterly-filing-dates/'>
+        <Link to="/documentation/2022/quarterly-filing-dates/">
           Quarterly HMDA Data Collection Timelines
         </Link>
       </li>
       <li>
-        <ExternalLink url='https://hmdahelp.consumerfinance.gov/knowledgebase/s/topic/0TOt0000000PFzGGAW/hmda-articles'>
+        <ExternalLink url="https://hmdahelp.consumerfinance.gov/knowledgebase/s/topic/0TOt0000000PFzGGAW/hmda-articles">
           HMDA Operations Knowledge Base
+        </ExternalLink>
+      </li>
+      <li>
+        <ExternalLink url="https://cfpb.github.io/hmda-platform">
+          HMDA Platform API
         </ExternalLink>
       </li>
     </ul>
