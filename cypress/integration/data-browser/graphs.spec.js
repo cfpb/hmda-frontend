e0822351353be62db9240bf6c2e72f9e833ee740
michaeldremy <michaeldremy@gmail.com>
2022-09-15 08:15:07 -0700

    [Cypress] 2 out of 7 tests are failing for graphs. Need to re-write broken tests and add more tests to cover tabs.

--- cypress/integration/data-browser/graphs.spec.js
+++ cypress/integration/data-browser/graphs.spec.js
@@ -65,107 +65,107 @@ describe.skip("General Tests", () => {
   });
 });
 
-describe.skip("Graph Specific tests", () => {
+describe("Graph Specific tests", () => {
   it("Select first graph from API and then switch to a different graph", () => {
-    cy.visit(`${baseURLToVisit}/data-browser/graphs/quarterly`);
+    cy.visit(`${baseURLToVisit}/data-browser/graphs/quarterly`)
     cy.get(".react-select__graph__control")
       .click(0, 0, { force: true })
       .find('[class*="-singleValue"]')
       .eq(0)
+      .click(0, 0, { force: true })
       .contains("How has the number of applications changed?")
-      .click(0, 0, { force: true });
     // Change to a different graph
     cy.get(".react-select__graph__control")
       .click(0, 0, { force: true })
-      .get('[class*="-menu"]')
-      .find('[class*="-option"]')
-      .eq(11)
-      .contains("For FHA loans, how has median DTI differed by race/ethnicity?")
-      .click(0, 0, { force: true });
+      .find(".react-select__graph__menu-list")
+      .find("#react-select-2-option-0-0")
+      .click(0, 0, { force: true })
+      .contains("How has the median CLTV changed?")
+      .click(0, 0, { force: true })
   });
 
-  it("Selects first option from graph drop-down, changes periods, update url with selected periods, clicks 'Show All Quarters' button and update url with default periods", () => {
-    // Choose first option from drop-down
-    cy.get(".react-select__graph__control")
-      .click(0, 0, { force: true })
-      .get('[class*="-menu"]')
-      .find('[class*="-option"]')
-      .eq(0)
-      .click(0, 0, { force: true })
-      .find('[class*="-singleValue"]')
-      .contains("How has the number of applications changed?")
-    // Selects 2nd option from period selector start
-    cy.get(".react-select__period_start__control")
-      .click(0, 0, { force: true })
-      .get('[class*="-menu"]')
-      .find('[class*="-option"]')
-      .eq(1)
-      .contains("2018-Q2")
-      .click(0, 0, { force: true });
-    // Selects 6th option from period selector end
-    cy.get(".react-select__period_end__control")
-      .click(0, 0, { force: true })
-      .get('[class*="-menu"]')
-      .find('[class*="-option"]')
-      .eq(5)
-      .contains("2019-Q4")
-      .click(0, 0, { force: true });
-    // Update URL with period selection and series
-    let urlUpdate = buildURL(
-      baseURLToVisit,
-      "applications",
-      "2018-Q2",
-      "2019-Q4",
-      firstGraphEndpoint.series
-    );
-    cy.url("include", urlUpdate);
-    cy.url("eq", urlUpdate);
-    // Clicks `Show All Quarters` button
-    cy.get(".period-wrapper").get(".reset").click();
-    // Update URL with period selection and series
-    urlUpdate = buildURL(
-      baseURLToVisit,
-      "applications",
-      "2018-Q1",
-      "2021-Q4",
-      firstGraphEndpoint.series
-    );
-    cy.url("include", urlUpdate);
-    cy.url("eq", urlUpdate);
-  });
+  // it("Selects first option from graph drop-down, changes periods, update url with selected periods, clicks 'Show All Quarters' button and update url with default periods", () => {
+  //   // Choose first option from drop-down
+  //   cy.get(".react-select__graph__control")
+  //     .click(0, 0, { force: true })
+  //     .get('[class*="-menu"]')
+  //     .find('[class*="-option"]')
+  //     .eq(0)
+  //     .click(0, 0, { force: true })
+  //     .find('[class*="-singleValue"]')
+  //     .contains("How has the number of applications changed?")
+  //   // Selects 2nd option from period selector start
+  //   cy.get(".react-select__period_start__control")
+  //     .click(0, 0, { force: true })
+  //     .get('[class*="-menu"]')
+  //     .find('[class*="-option"]')
+  //     .eq(1)
+  //     .contains("2018-Q2")
+  //     .click(0, 0, { force: true });
+  //   // Selects 6th option from period selector end
+  //   cy.get(".react-select__period_end__control")
+  //     .click(0, 0, { force: true })
+  //     .get('[class*="-menu"]')
+  //     .find('[class*="-option"]')
+  //     .eq(5)
+  //     .contains("2019-Q4")
+  //     .click(0, 0, { force: true });
+  //   // Update URL with period selection and series
+  //   let urlUpdate = buildURL(
+  //     baseURLToVisit,
+  //     "applications",
+  //     "2018-Q2",
+  //     "2019-Q4",
+  //     firstGraphEndpoint.series
+  //   );
+  //   cy.url("include", urlUpdate);
+  //   cy.url("eq", urlUpdate);
+  //   // Clicks `Show All Quarters` button
+  //   cy.get(".period-wrapper").get(".reset").click();
+  //   // Update URL with period selection and series
+  //   urlUpdate = buildURL(
+  //     baseURLToVisit,
+  //     "applications",
+  //     "2018-Q1",
+  //     "2021-Q4",
+  //     firstGraphEndpoint.series
+  //   );
+  //   cy.url("include", urlUpdate);
+  //   cy.url("eq", urlUpdate);
+  // });
 
-  it("De-select and re-select a series and have the URL change/update the in the UI", () => {
-    let urlUpdate;
-    cy.visit(`${baseURLToVisit}/data-browser/graphs/quarterly`);
-    cy.get(".highcharts-color-0 > text").click(0, 0, { force: true });
-    urlUpdate = buildURL(baseURLToVisit, "applications", "2018-Q1", "2021-Q4", [
-      "Conventional Non-Conforming",
-      "FHA",
-      "HELOC",
-      "RHS/FSA",
-      "VA",
-    ]);
-    cy.url("include", urlUpdate);
-    cy.url("eq", urlUpdate);
-    cy.get(".highcharts-color-0 > text").click(0, 0, { force: true });
-    urlUpdate = buildURL(baseURLToVisit, "applications", "2018-Q1", "2021-Q4", [
-      "Conventional Conforming",
-      "Conventional Non-Conforming",
-      "FHA",
-      "HELOC",
-      "RHS/FSA",
-      "VA",
-    ]);
-    cy.url("include", urlUpdate);
-    cy.url("eq", urlUpdate);
-  });
+  // it("De-select and re-select a series and have the URL change/update the in the UI", () => {
+  //   let urlUpdate;
+  //   cy.visit(`${baseURLToVisit}/data-browser/graphs/quarterly`);
+  //   cy.get(".highcharts-color-0 > text").click(0, 0, { force: true });
+  //   urlUpdate = buildURL(baseURLToVisit, "applications", "2018-Q1", "2021-Q4", [
+  //     "Conventional Non-Conforming",
+  //     "FHA",
+  //     "HELOC",
+  //     "RHS/FSA",
+  //     "VA",
+  //   ]);
+  //   cy.url("include", urlUpdate);
+  //   cy.url("eq", urlUpdate);
+  //   cy.get(".highcharts-color-0 > text").click(0, 0, { force: true });
+  //   urlUpdate = buildURL(baseURLToVisit, "applications", "2018-Q1", "2021-Q4", [
+  //     "Conventional Conforming",
+  //     "Conventional Non-Conforming",
+  //     "FHA",
+  //     "HELOC",
+  //     "RHS/FSA",
+  //     "VA",
+  //   ]);
+  //   cy.url("include", urlUpdate);
+  //   cy.url("eq", urlUpdate);
+  // });
 
-  it("URL loads with specific set of series and it's reflected in the UI", () => {
-    let series = ["FHA", "HELOC", "VA"];
-    cy.visit(
-      `${baseURLToVisit}/data-browser/graphs/quarterly/applications?periodLow=2018-Q1&periodHigh=2021-Q4&visibleSeries=FHA,HELOC,VA`
-    );
-    cy.wait(1000);
-    cy.url("include", series);
-  });
+  // it("URL loads with specific set of series and it's reflected in the UI", () => {
+  //   let series = ["FHA", "HELOC", "VA"];
+  //   cy.visit(
+  //     `${baseURLToVisit}/data-browser/graphs/quarterly/applications?periodLow=2018-Q1&periodHigh=2021-Q4&visibleSeries=FHA,HELOC,VA`
+  //   );
+  //   cy.wait(1000);
+  //   cy.url("include", series);
+  // });
 });
