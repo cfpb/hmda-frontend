eb7ce251965a0a5067fe52b6b614652c6090ec03
michaeldremy <michaeldremy@gmail.com>
2022-10-07 13:26:30 -0700

    [Graphs] Users are able to download filer loan and applicaton counts table via a CSV file.

--- src/common/SimpleSortTable.jsx
+++ src/common/SimpleSortTable.jsx
@@ -1,94 +1,162 @@
-import { flexRender, getCoreRowModel, getSortedRowModel, useReactTable } from '@tanstack/react-table';
-import { useState } from 'react';
-import Pills from './Pills';
-import './SimpleSortTable.css';
+import {
+  flexRender,
+  getCoreRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table"
+import fileSaver from "file-saver"
+import { useState, useRef } from "react"
+import Pills from "./Pills"
+import "./SimpleSortTable.css"
+import { buildCSVRows } from "../data-publication/reports/tables/AggregateUtils"
 
-const SimpleSortTable = props => {
-  const { data, columns, customFooter, initialSort: { sort, setSort } } = props;
+const SimpleSortTable = (props) => {
+  const {
+    data,
+    columns,
+    customFooter,
+    initialSort: { sort, setSort },
+    year,
+  } = props
 
-  const [sorting, setSorting] = useState(sort || []);
+  const [sorting, setSorting] = useState(sort || [])
 
-  const onSortingChange = changeFn => {
-    setSorting(changeFn);
-    setSort?.call(undefined, changeFn);
-  };
+  const tableRef = useRef()
+
+  // Generates a CSV file from the table on the page
+  const generateCSV = () => {
+    let theCSV = ""
+
+    if (!tableRef.current) return
+
+    // Extracting table header innerHTML because the table headers contained sorting icons
+    const tHeadRows = tableRef.current.tHead.rows.item("0").cells
+    const rowArray = Array.from(tHeadRows)
+    const rowHeaders = rowArray.map(
+      (header) => header.children.item("0").children.item("0").innerHTML
+    )
+
+    theCSV = theCSV + rowHeaders.join(",") + "\n"
+
+    // Extracting table body rows
+    const tBodyRows = tableRef.current.tBodies[0].rows
+    theCSV = theCSV + buildCSVRows(tBodyRows, "body")
+
+    // Extracting table footer rows
+    const tFootRows = tableRef.current.tFoot.rows
+    theCSV = theCSV + buildCSVRows(tFootRows, "foot")
+
+    // Downloads CSV file
+    fileSaver.saveAs(
+      new Blob([theCSV], { type: "text/csv;charset=utf-16" }),
+      `${year} Quarterly Filer Loan and Application Counts.csv`
+    )
+  }
+
+  const onSortingChange = (changeFn) => {
+    setSorting(changeFn)
+    setSort?.call(undefined, changeFn)
+  }
 
   const table = useReactTable({
     data,
     columns,
     state: {
-      sorting
+      sorting,
     },
     enableSortingRemoval: false,
     onSortingChange,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
-  });
+  })
 
-  const updateSortSelection = updatedSelection => onSortingChange(old => updatedSelection);
+  const updateSortSelection = (updatedSelection) =>
+    onSortingChange((old) => updatedSelection)
 
-  const getSortArrow = sorted => {
-    if (sorted === 'asc') return '\u2191';
-    if (sorted === 'desc') return '\u2193';
-    return '\u2195';
-  };
+  const getSortArrow = (sorted) => {
+    if (sorted === "asc") return "\u2191"
+    if (sorted === "desc") return "\u2193"
+    return "\u2195"
+  }
 
-  const pills = sort.map(s => {
-    const label = columns.find(({ accessorKey }) => accessorKey === s.id)?.header.concat(s.desc ? ' \u2193' : ' \u2191');
+  const pills = sort.map((s) => {
+    const label = columns
+      .find(({ accessorKey }) => accessorKey === s.id)
+      ?.header.concat(s.desc ? " \u2193" : " \u2191")
     return { ...s, label }
-  });
+  })
 
-  return <div className="simple-sort-table-container">
-    <div>
-      <ul>
-        <li>Table is sortable by selecting the column headers.</li>
-        <li>Hold Shift and select multiple column headers to sort by multiple fields.</li>
-      </ul>
+  return (
+    <div className="simple-sort-table-container">
       <div>
-        <Pills values={pills} onChange={updateSortSelection} />
+        <ul>
+          <li>Table is sortable by selecting the column headers.</li>
+          <li>
+            Hold Shift and select multiple column headers to sort by multiple
+            fields.
+          </li>
+        </ul>
+
+        {data && (
+          <button
+            onClick={() => generateCSV()}
+            style={{ margin: "5px 0px 15px 0px" }}
+          >
+            Save as CSV
+          </button>
+        )}
+
+        <div>
+          <Pills values={pills} onChange={updateSortSelection} />
+        </div>
       </div>
-    </div>
-    <table className="simple-sort-table">
-      <thead>
-        {
-          table.getHeaderGroups().map(headerGroup => (
+      <table className="simple-sort-table" ref={tableRef}>
+        <thead>
+          {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => {
-                const clickableHeader = header.column.getCanSort() ? 'cursor-pointer select-none' : null;
+              {headerGroup.headers.map((header) => {
+                const clickableHeader = header.column.getCanSort()
+                  ? "cursor-pointer select-none"
+                  : null
                 return (
                   <th key={header.id} colSpan={header.colSpan}>
                     {header.isPlaceholder ? null : (
-                      <div className={clickableHeader}
-                        onClick={header.column.getToggleSortingHandler()}>
+                      <div
+                        className={clickableHeader}
+                        onClick={header.column.getToggleSortingHandler()}
+                      >
                         <span>
-                          {flexRender(header.column.columnDef.header, header.getContext())}
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
                         </span>
-                        <span className="sort-icon">{getSortArrow(header.column.getIsSorted())}</span>
+                        <span className="sort-icon">
+                          {getSortArrow(header.column.getIsSorted())}
+                        </span>
                       </div>
                     )}
                   </th>
                 )
               })}
             </tr>
-          ))
-        }
-      </thead>
-      <tbody>
-        {table.getRowModel().rows.map(row => (
-          <tr key={row.id}>
-            {row.getVisibleCells().map(cell => (
-              <td key={cell.id}>
-                {cell.getContext().getValue()?.toLocaleString()}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-      <tfoot>
-        {customFooter}
-      </tfoot>
-    </table>
-  </div>;
-};
+          ))}
+        </thead>
+        <tbody>
+          {table.getRowModel().rows.map((row) => (
+            <tr key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <td key={cell.id}>
+                  {cell.getContext().getValue()?.toLocaleString()}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>{customFooter}</tfoot>
+      </table>
+    </div>
+  )
+}
 
-export default SimpleSortTable;
+export default SimpleSortTable
--- src/data-browser/graphs/quarterly/QuarterlyFilersTable.jsx
+++ src/data-browser/graphs/quarterly/QuarterlyFilersTable.jsx
@@ -1,98 +1,131 @@
-import { useEffect, useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import LoadingIcon from '../../../common/LoadingIcon';
-import SimpleSortTable from '../../../common/SimpleSortTable';
-import { institutions } from '../slice';
-import './QuarterlyFilersTable.css';
+import { useEffect, useMemo, useRef } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import LoadingIcon from "../../../common/LoadingIcon"
+import SimpleSortTable from "../../../common/SimpleSortTable"
+import { institutions } from "../slice"
+import "./QuarterlyFilersTable.css"
 
-const QuarterlyFilersTable = props => {
-  const dispatch = useDispatch();
-  const [year, past] = [new Date().getFullYear(), 3];
-  const { data, loading, sort } = useSelector(state => state.institutions);
-  const pastYears = [...Array(past).keys()].map(i => `${year - i - 1}`);
+const QuarterlyFilersTable = (props) => {
+  const dispatch = useDispatch()
+  const [year, past] = [new Date().getFullYear(), 3]
+  const { data, loading, sort } = useSelector((state) => state.institutions)
+  const pastYears = [...Array(past).keys()].map((i) => `${year - i - 1}`)
 
   useEffect(() => {
     if (!data) {
-      dispatch(institutions.fetchInstitutionLars({ year, past }));
-      dispatch(institutions.updateSort([{ id: 'agency', desc: false }, { id: 'name', desc: false }]));
+      dispatch(institutions.fetchInstitutionLars({ year, past }))
+      dispatch(
+        institutions.updateSort([
+          { id: "agency", desc: false },
+          { id: "name", desc: false },
+        ])
+      )
     }
-  }, [dispatch, data]);
+  }, [dispatch, data])
 
-  const setSort = sortUpdateFn => dispatch(institutions.updateSort(sortUpdateFn(sort)));
+  const setSort = (sortUpdateFn) =>
+    dispatch(institutions.updateSort(sortUpdateFn(sort)))
 
-  let content = <LoadingIcon />;
+  let content = <LoadingIcon />
 
   const tableColumns = useMemo(() => {
-    const countsColumns = pastYears.map(accessorKey => {
+    const countsColumns = pastYears.map((accessorKey) => {
       return {
         header: accessorKey,
         accessorKey,
-        sortingFn: 'alphanumeric',
-      };
-    });
+        sortingFn: "alphanumeric",
+      }
+    })
 
-    return [{
-      header: 'Institution',
-      accessorKey: 'name',
-      sortingFn: 'text',
-    }, {
-      header: 'LEI',
-      accessorKey: 'lei',
-      sortingFn: 'text',
-    }, {
-      header: 'Agency',
-      accessorKey: 'agency',
-      sortingFn: 'text',
-    }, ...countsColumns];
-  });
+    return [
+      {
+        header: "Institution",
+        accessorKey: "name",
+        sortingFn: "text",
+      },
+      {
+        header: "LEI",
+        accessorKey: "lei",
+        sortingFn: "text",
+      },
+      {
+        header: "Agency",
+        accessorKey: "agency",
+        sortingFn: "text",
+      },
+      ...countsColumns,
+    ]
+  })
 
-  if (loading === 'succeeded' && data) {
+  if (loading === "succeeded" && data) {
     const tableData = data.quarterly.map(({ name, lei, agency, larCounts }) => {
-      let counts = {};
-      larCounts.forEach(ts => {
-        counts[ts.year] = ts.count;
-      });
+      let counts = {}
+      larCounts.forEach((ts) => {
+        counts[ts.year] = ts.count
+      })
       return {
         name: name.toUpperCase(),
         lei,
         agency,
-        ...counts
+        ...counts,
       }
-    });
+    })
 
-    const quarterlySums = {};
-    pastYears.forEach(yr => {
+    const quarterlySums = {}
+    pastYears.forEach((yr) => {
       quarterlySums[yr] = data.quarterly.reduce((prev, curr) => {
-        const tsLar = curr.larCounts.find(ts => ts.year === yr);
-        return tsLar ? prev + tsLar.count : prev;
-      }, 0);
-    });
+        const tsLar = curr.larCounts.find((ts) => ts.year === yr)
+        return tsLar ? prev + tsLar.count : prev
+      }, 0)
+    })
 
-    const customFooter = <>
-      <tr>
-        <th colSpan="3" style={{ textAlign: 'right' }}>Total of Quarterly Filers</th>
-        {pastYears.map(yr => {
-          return <td key={yr}>{quarterlySums[yr].toLocaleString()}</td>
-        })}
-      </tr>
-      <tr>
-        <th colSpan="3" style={{ textAlign: 'right' }}>Total of All Filers</th>
-        {pastYears.map(yr => {
-          return <td key={yr}>{(data.yearly.find(yearlyCount => yearlyCount.year === yr) || { count: 0 }).count.toLocaleString()}</td>
-        })}
-      </tr>
-    </>;
-    content = <SimpleSortTable columns={tableColumns} data={tableData} customFooter={customFooter} initialSort={{ sort, setSort }} />;
+    const customFooter = (
+      <>
+        <tr>
+          <th colSpan="3" style={{ textAlign: "right" }}>
+            Total of Quarterly Filers
+          </th>
+          {pastYears.map((yr) => {
+            return <td key={yr}>{quarterlySums[yr].toLocaleString()}</td>
+          })}
+        </tr>
+        <tr>
+          <th colSpan="3" style={{ textAlign: "right" }}>
+            Total of All Filers
+          </th>
+          {pastYears.map((yr) => {
+            return (
+              <td key={yr}>
+                {(
+                  data.yearly.find(
+                    (yearlyCount) => yearlyCount.year === yr
+                  ) || { count: 0 }
+                ).count.toLocaleString()}
+              </td>
+            )
+          })}
+        </tr>
+      </>
+    )
+    content = (
+      <SimpleSortTable
+        columns={tableColumns}
+        data={tableData}
+        customFooter={customFooter}
+        initialSort={{ sort, setSort }}
+        year={year}
+      />
+    )
   }
 
   return (
     <div className="quarterly-filers-table">
-      <h2 className="table-heading">{year} Quarterly Filer Loan and Application Counts</h2>
-      <div className="table-container">
-        {content}
-      </div>
+      <h2 className="table-heading">
+        {year} Quarterly Filer Loan and Application Counts
+      </h2>
+      <div className="table-container">{content}</div>
     </div>
-  );
-};
+  )
+}
 
-export default QuarterlyFilersTable;
+export default QuarterlyFilersTable
