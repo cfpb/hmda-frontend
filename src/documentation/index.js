3f219c0a5ed998d97ba2fda8b4ba23dde03a7306
Meis <meissadia@gmail.com>
2022-11-03 17:18:40 -0600

    [Documentation] index.js - apply formatting

--- src/documentation/index.js
+++ src/documentation/index.js
@@ -1,35 +1,35 @@
-import React from "react"
-import { Switch, Redirect, Route, Link } from "react-router-dom"
+import React from 'react'
+import { Switch, Redirect, Route, Link } from 'react-router-dom'
 
-import Heading from "../common/Heading.jsx"
-import NotFound from "../common/NotFound"
-import YearSelector from "../common/YearSelector.jsx"
-import Home from "./Home"
-import DynamicRenderer from "./DynamicRenderer"
-import { isBadYear } from "./markdownUtils"
-import FAQs from "./FAQs.jsx"
-import FigLinks from "./FigLinks.jsx"
-import Publications from "./publications"
-import ModifiedLar from "./publications/ModifiedLar.jsx"
-import ADReports from "./publications/ADReports.jsx"
-import SnapshotDynamic from "./publications/SnapshotDynamic.jsx"
-import Tools from "./tools"
-import DataBrowser from "./tools/DataBrowser.jsx"
-import RateSpread from "./tools/RateSpread.jsx"
-import CheckDigit from "./tools/CheckDigit.jsx"
-import LARFT from "./tools/LARFT.jsx"
-import FFVT from "./tools/FFVT.jsx"
-import { withAppContext } from "../common/appContextHOC"
-import { DOCS_YEARS } from "../common/constants/years"
+import Heading from '../common/Heading.jsx'
+import NotFound from '../common/NotFound'
+import YearSelector from '../common/YearSelector.jsx'
+import Home from './Home'
+import DynamicRenderer from './DynamicRenderer'
+import { isBadYear } from './markdownUtils'
+import FAQs from './FAQs.jsx'
+import FigLinks from './FigLinks.jsx'
+import Publications from './publications'
+import ModifiedLar from './publications/ModifiedLar.jsx'
+import ADReports from './publications/ADReports.jsx'
+import SnapshotDynamic from './publications/SnapshotDynamic.jsx'
+import Tools from './tools'
+import DataBrowser from './tools/DataBrowser.jsx'
+import RateSpread from './tools/RateSpread.jsx'
+import CheckDigit from './tools/CheckDigit.jsx'
+import LARFT from './tools/LARFT.jsx'
+import FFVT from './tools/FFVT.jsx'
+import { withAppContext } from '../common/appContextHOC'
+import { DOCS_YEARS } from '../common/constants/years'
 
-import "./index.css"
-import ScrollToTop from "../common/ScrollToTop.jsx"
+import './index.css'
+import ScrollToTop from '../common/ScrollToTop.jsx'
 
 function makeCollectionPage(Component, heading, year, url) {
   return (
-    <div className="App Documentation">
-      <Link className="BackLink" to="/documentation/">
-        {"\u2b05"} DOCUMENTATION HOME
+    <div className='App Documentation'>
+      <Link className='BackLink' to='/documentation/'>
+        {'\u2b05'} DOCUMENTATION HOME
       </Link>
       <Heading type={1} headingText={heading}></Heading>
       <YearSelector year={year} url={url} years={DOCS_YEARS} />
@@ -39,14 +39,14 @@ function makeCollectionPage(Component, heading, year, url) {
 }
 
 const pageSlugs = {
-  "modified-lar": ModifiedLar,
-  "ad-reports": ADReports,
-  "snapshot-dynamic": SnapshotDynamic,
-  "data-browser": DataBrowser,
-  "rate-spread": RateSpread,
-  "check-digit": CheckDigit,
-  "lar-formatting": LARFT,
-  "file-format-verification": FFVT,
+  'modified-lar': ModifiedLar,
+  'ad-reports': ADReports,
+  'snapshot-dynamic': SnapshotDynamic,
+  'data-browser': DataBrowser,
+  'rate-spread': RateSpread,
+  'check-digit': CheckDigit,
+  'lar-formatting': LARFT,
+  'file-format-verification': FFVT,
 }
 
 const Documentation = ({ config }) => {
@@ -55,14 +55,14 @@ const Documentation = ({ config }) => {
       <Switch>
         <Redirect
           exact
-          from="/documentation"
+          from='/documentation'
           to={`/documentation/${config.defaultDocsPeriod}/`}
         />
 
         <Route
           exact
-          path="/documentation/:year/"
-          render={(props) => {
+          path='/documentation/:year/'
+          render={props => {
             const {
               url,
               params: { year },
@@ -71,7 +71,7 @@ const Documentation = ({ config }) => {
             if (isBadYear(year)) return <NotFound />
 
             return (
-              <div className="App Documentation">
+              <div className='App Documentation'>
                 <Home year={year} url={url} />
               </div>
             )
@@ -80,8 +80,8 @@ const Documentation = ({ config }) => {
 
         <Route
           exact
-          path="/documentation/:year/:collection/"
-          render={(props) => {
+          path='/documentation/:year/:collection/'
+          render={props => {
             const {
               url,
               params: { year, collection },
@@ -89,32 +89,32 @@ const Documentation = ({ config }) => {
 
             if (isBadYear(year)) return <NotFound />
 
-            if (collection === "faqs")
+            if (collection === 'faqs')
               return makeCollectionPage(
                 FAQs,
-                "Frequently Asked Questions",
+                'Frequently Asked Questions',
                 year,
                 url
               )
-            if (collection === "fig")
+            if (collection === 'fig')
               return makeCollectionPage(
                 FigLinks,
-                "Filing Instructions Guide (FIG)",
+                'Filing Instructions Guide (FIG)',
                 year,
                 url
               )
-            if (collection === "publications")
+            if (collection === 'publications')
               return makeCollectionPage(
                 Publications,
-                "HMDA Publications",
+                'HMDA Publications',
                 year,
                 url
               )
-            if (collection === "tools")
-              return makeCollectionPage(Tools, "HMDA Tools", year, url)
+            if (collection === 'tools')
+              return makeCollectionPage(Tools, 'HMDA Tools', year, url)
 
             return (
-              <div className="App Documentation">
+              <div className='App Documentation'>
                 <DynamicRenderer year={year} slug={collection} props={props} />
               </div>
             )
@@ -123,8 +123,8 @@ const Documentation = ({ config }) => {
 
         <Route
           exact
-          path="/documentation/:year/:collection/:slug/"
-          render={(props) => {
+          path='/documentation/:year/:collection/:slug/'
+          render={props => {
             const {
               url,
               params: { year, collection, slug },
@@ -133,19 +133,19 @@ const Documentation = ({ config }) => {
 
             if (
               isBadYear(year) ||
-              (collection !== "publications" && collection !== "tools") ||
+              (collection !== 'publications' && collection !== 'tools') ||
               !Component
             ) {
               return <NotFound />
             }
 
             return (
-              <div className="App Documentation">
+              <div className='App Documentation'>
                 <Link
-                  className="BackLink"
+                  className='BackLink'
                   to={`/documentation/${year}/${collection}/`}
                 >
-                  {"\u2b05"} HMDA {collection}
+                  {'\u2b05'} HMDA {collection}
                 </Link>
                 <Component
                   year={year}
