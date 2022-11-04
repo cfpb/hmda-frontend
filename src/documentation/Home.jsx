4691864872dd33f6c00c5f92f540d21c7b40f77f
michaeldremy <michaeldremy@gmail.com>
2022-11-04 13:31:28 -0700

    [Documentation] Updated updates-notes README and added new external link to the bottom of documentation page, list now includes a link to HMDA File Serving API.

--- src/documentation/Home.jsx
+++ src/documentation/Home.jsx
@@ -12,28 +12,42 @@ import { ExternalLink } from '../common/ExternalLink'
 const Home = props => {
   const { year, url } = props
   return (
-    <div className="home">
-      <div className="intro">
-        <Header type={1} headingText="HMDA Documentation">
-          <p className="lead">A collection of HMDA Documentation Resources</p>
+    <div className='home'>
+      <div className='intro'>
+        <Header type={1} headingText='HMDA Documentation'>
+          <p className='lead'>A collection of HMDA Documentation Resources</p>
         </Header>
       </div>
       <YearSelector year={year} url={url} years={DOCS_YEARS} />
       <div>
-        <h2><Link to={`/documentation/${year}/faqs/`}>Frequently Asked Questions</Link></h2>
-        <FAQs year={year}/>
+        <h2>
+          <Link to={`/documentation/${year}/faqs/`}>
+            Frequently Asked Questions
+          </Link>
+        </h2>
+        <FAQs year={year} />
       </div>
       <div>
-        <h2><Link to={`/documentation/${year}/fig/`}>Filing Instructions Guide (FIG)</Link></h2>
-        <FigLinks year={year}/>
+        <h2>
+          <Link to={`/documentation/${year}/fig/`}>
+            Filing Instructions Guide (FIG)
+          </Link>
+        </h2>
+        <FigLinks year={year} />
       </div>
       <div>
-        <h2><Link to={`/documentation/${year}/publications/`}>HMDA Publications</Link></h2>
-        <Publications year={year}/>
+        <h2>
+          <Link to={`/documentation/${year}/publications/`}>
+            HMDA Publications
+          </Link>
+        </h2>
+        <Publications year={year} />
       </div>
       <div>
-        <h2><Link to={`/documentation/${year}/tools/`}>HMDA Tools</Link></h2>
-        <Tools year={year}/>
+        <h2>
+          <Link to={`/documentation/${year}/tools/`}>HMDA Tools</Link>
+        </h2>
+        <Tools year={year} />
       </div>
       <div>
         <h2>HMDA APIs</h2>
@@ -76,6 +90,12 @@ const Home = props => {
             <ExternalLink
               url='https://cfpb.github.io/hmda-platform/#check-digit'
               text='HMDA Check Digit API'
+            />
+          </li>
+          <li>
+            <ExternalLink
+              url='https://cfpb.github.io/hmda-platform/#hmda-file-serving'
+              text='HMDA File Serving API'
             />
           </li>
         </ul>
