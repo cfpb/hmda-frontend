06faa51d9ef3363e51301e3c27b577bffa79cb9b
Meis <meissadia@gmail.com>
2022-12-13 11:59:52 -0700

    [olarft] Bug: Make adjustFocusToTriggerRerender less jumpy

--- src/tools/larft/hooks/adjustFocusToTriggerRerender.jsx
+++ src/tools/larft/hooks/adjustFocusToTriggerRerender.jsx
@@ -1,22 +1,22 @@
+import { scrollToFileActions } from '../utils/common'
+
 // Hacky attempt to fix column width after a cleared columnFilter
 export const adjustFocusToTriggerRerender = (selector) => setTimeout(() => {
   const arr = [
     ...document.querySelectorAll(selector + ' .react-fluid-table-row'),
   ]
+  
   const middleIdx = parseInt(arr.length / 2)
 
   setTimeout(() => {
-    arr[middleIdx].scrollIntoView({
-      behavior: 'smooth',
+    arr[middleIdx]?.scrollIntoView({
       block: 'center',
       inline: 'center',
     })
   }, 0)
-
-  const arr2 = [...document.querySelectorAll(selector + ' #header-record-identifier')]
-  arr2.forEach(el => el.scrollIntoView({
-    behavior: 'smooth',
-    block: 'center',
-    inline: 'center',
-  }))
+  
+  setTimeout(() => {
+    scrollToFileActions()
+  }, 0)
+  
 }, 0)
