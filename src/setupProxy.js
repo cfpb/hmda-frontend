1bab38206e4a868afda3f0b569bc80c7236c0fc6
ojbravo <ojbravo@gmail.com>
2023-04-14 06:49:37 -0600

    Modified setupProxy.js to connect to local filing Platform

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ const isCI = () => process.env.REACT_APP_ENVIRONMENT === 'CI'
 
 APIS = [
   { endpoint: `/v2/filing/`, target:  'http://localhost:8080' },       // hmda-filing-api
-  { endpoint: `/v2/admin/`,  target:  'http://localhost:8081' },       // hmda-admin-api
+  { endpoint: `/v2/admin/`,  target:  'http://localhost:8081/' },       // hmda-admin-api
   { endpoint: `/v2/public/uli/`, target:  'http://localhost:9091' },   // check-digit
   { endpoint: `/v2/public/`, target:  'http://localhost:8082' },       // hmda-public-api
   { endpoint: `/public/rateSpread`, target:  'http://localhost:9095' } // ratespread-calculator
