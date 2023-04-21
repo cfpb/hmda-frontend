a501de46350271bc079d1bd4b58487bcc4755960
michaeldremy <michaeldremy@gmail.com>
2023-04-21 10:23:17 -0700

    [Ratespread] Updated last paragraph on the ratespread calc tool via #1778 ticket.

--- src/tools/rate-spread/AppIntro.jsx
+++ src/tools/rate-spread/AppIntro.jsx
@@ -4,11 +4,15 @@ import Heading from '../../common/Heading.jsx'
 
 const AppIntro = () => {
   return (
-    <Heading type={1} style={{marginBottom: 0}} headingText="Rate Spread Calculator">
-      <p className="font-lead">
+    <Heading
+      type={1}
+      style={{ marginBottom: 0 }}
+      headingText='Rate Spread Calculator'
+    >
+      <p className='font-lead'>
         This calculator provides rate spreads for HMDA reportable loans with a
         final action date on or after January 1st, 2018. Use the{' '}
-        <a href="https://www.ffiec.gov/ratespread/newcalc.aspx">
+        <a href='https://www.ffiec.gov/ratespread/newcalc.aspx'>
           prior rate spread calculator
         </a>{' '}
         for loans with a final action date before January 1st, 2018.
@@ -20,36 +24,51 @@ const AppIntro = () => {
         “Average Prime Offer Rates”{' '}
         <a
           download
-          href="https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/YieldTableFixed.txt"
+          href='https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/YieldTableFixed.txt'
         >
           fixed table
         </a>{' '}
         or{' '}
         <a
-          href="https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/YieldTableAdjustable.txt"
+          href='https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/YieldTableAdjustable.txt'
           download
         >
           adjustable table
-        </a>, action taken, amortization type, lock-in date, APR, fixed term
-        (loan maturity) or variable term (initial fixed-rate period), and
-        reverse mortgage.
+        </a>
+        , action taken, amortization type, lock-in date, APR, fixed term (loan
+        maturity) or variable term (initial fixed-rate period), and reverse
+        mortgage.
       </p>
       <p>
         <a
-          href="https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/SurveyTable.csv"
+          href='https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/SurveyTable.csv'
           download
         >
           Mortgage rate survey data
         </a>{' '}
-        used to calculate rate spreads for loans reportable under HMDA is available for download. The data are provided through ICE Mortgage TechnologyTM. ICE Data as a Service is provided by ICE Mortgage TechnologyTM, part of Intercontinental Exchange, Inc.
+        used to calculate rate spreads for loans reportable under HMDA is
+        available for download. The data are provided through ICE Mortgage
+        TechnologyTM. ICE Data as a Service is provided by ICE Mortgage
+        TechnologyTM, part of Intercontinental Exchange, Inc.
       </p>
       <p>
-        <Link to="/tools/rate-spread/requirements">Data requirements</Link> for the rate spread calculator are provided in accordance with Regulation C effective January 1st, 2018.
+        <Link to='/tools/rate-spread/requirements'>Data requirements</Link> for
+        the rate spread calculator are provided in accordance with Regulation C
+        effective January 1st, 2018.
       </p>
       <p>
-        The <Link to="/tools/rate-spread/methodology">Current Methodology</Link> for determining Average Prime Offer Rates is provided. APOR rates published prior to April 24, 2023 used an <Link to="/tools/rate-spread/methodology-alt">Alternate Methodology</Link> which is also provided.
-        </p>
-
+        The <Link to='/tools/rate-spread/methodology'>current methodology</Link>{' '}
+        for determining Average Prime Offer Rates is provided. APOR rates
+        published prior to April 24, 2023 used an{' '}
+        <Link to='/tools/rate-spread/methodology-alt'>
+          alternate methodology
+        </Link>{' '}
+        and{' '}
+        <a href='https://s3.amazonaws.com/cfpb-hmda-public/prod/apor/SurveyTable-legacy.csv'>
+          alternate rate survey data
+        </a>{' '}
+        which are also provided.
+      </p>
     </Heading>
   )
 }
