873a1c33c9286acc9bbf83d16185761a3010f3d6
michaeldremy <michaeldremy@gmail.com>
2023-08-09 12:41:18 -0700

    [Tools Page] Introduced more obvious cards for each of the tools and improved mobile responsiveness

--- src/tools/Home.jsx
+++ src/tools/Home.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
-import Heading from '../common/Heading.jsx'
-import NewIndicator from '../homepage/NewIndicator.jsx'
+import React from "react"
+import { Link } from "react-router-dom"
+import Heading from "../common/Heading.jsx"
+import NewIndicator from "../homepage/NewIndicator.jsx"
+
+import "./Home.css"
 
 const Home = () => {
   return (
     <div className='grid home'>
       <Heading
         type={1}
-        style={{ marginBottom: '3em' }}
+        style={{ marginBottom: "0em" }}
         headingText='HMDA Tools'
         paragraphText='Here you can find various tools to assist you in getting your HMDA LAR ready for filing.'
       />
 
       <div className='card-container'>
-        <div className='card'>
+        <Link to='/tools/rate-spread' className='card tools-card'>
           <Heading
-            headingLink='/tools/rate-spread'
             headingText='Rate Spread'
             paragraphText='This calculator provides rate spreads for HMDA reportable loans
-              with a final action date on or after January 1st, 2018.'
+            with a final action date on or after January 1st, 2018.'
             type={3}
           />
-        </div>
+        </Link>
 
-        <div className='card'>
+        <div
+          className='card tools-card tools-card-no-hover-change'
+          style={{ cursor: "initial" }}
+        >
           <Heading
-            headingLink=''
             headingText='LAR Formatting'
             paragraphText='The LAR Formatting tools are intended to help financial
               institutions, typically those with small volumes of covered loans
@@ -36,7 +40,8 @@ const Home = () => {
             <ul>
               <li>
                 <a href='/tools/online-lar-formatting'>
-                  Online LAR Formatting <NewIndicator />
+                  Online LAR Formatting{" "}
+                  <NewIndicator id='tools-card-indicator' />
                 </a>
               </li>
               <li>
@@ -46,20 +51,18 @@ const Home = () => {
           </Heading>
         </div>
 
-        <div className='card'>
+        <Link to='/tools/file-format-verification' className='card tools-card'>
           <Heading
-            headingLink='/tools/file-format-verification'
             headingText='File Format Verification'
             paragraphText='The File Format Verification Tool (FFVT) is a resource for testing
               whether your file meets certain formatting requirements specified
               in the HMDA Filing Instructions Guide.'
             type={3}
           />
-        </div>
+        </Link>
 
-        <div className='card'>
+        <Link to='/tools/check-digit' className='card tools-card'>
           <Heading
-            headingLink='/tools/check-digit'
             headingText='Check Digit'
             paragraphText='You can use this tool for two functions. The first is to generate
               a two character check digit when you enter a Legal Entity
@@ -68,7 +71,7 @@ const Home = () => {
               complete Universal Loan Identifier (ULI) you enter.'
             type={3}
           />
-        </div>
+        </Link>
       </div>
     </div>
   )
