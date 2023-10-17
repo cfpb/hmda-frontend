3ae961eac9ce78e5d3e601346dc510dd8cbe84f8
michaeldremy <michaeldremy@gmail.com>
2023-10-17 10:32:09 -0700

    [Config] Introduced the ability to proxy through the vite config

--- vite.config.js
+++ vite.config.js
@@ -2,24 +2,38 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import svgr from "vite-plugin-svgr"
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import dns from "dns"
 
+dns.setDefaultResultOrder("verbatim")
 
 export default () => {
   return defineConfig({
     plugins: [react(), svgr()],
-    optimizeDeps: {
-      esbuildOptions: {
-        // Node.js global to browser globalThis
-        define: {
-          global: "globalThis",
+    server: {
+      port: "3000",
+      proxy: {
+        "/": {
+          target: "", // Enter the domain to proxy to, this is Vite's way of proxing to DEV or PROD
+          changeOrigin: true,
+          cookiePathRewrite: {
+            "*": "/",
+          },
         },
-        // Enable esbuild polyfill plugins
-        plugins: [
-          NodeGlobalsPolyfillPlugin({
-            buffer: true,
-          }),
-        ],
       },
     },
+    // optimizeDeps: {
+    //   esbuildOptions: {
+    //     // Node.js global to browser globalThis
+    //     define: {
+    //       global: "globalThis",
+    //     },
+    //     // Enable esbuild polyfill plugins
+    //     plugins: [
+    //       NodeGlobalsPolyfillPlugin({
+    //         buffer: true,
+    //       }),
+    //     ],
+    //   },
+    // },
   })
 }
