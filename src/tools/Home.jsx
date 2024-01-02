79623cb28258606c8e1d5b0241437eeea33d9c4e
michaeldremy <michaeldremy@gmail.com>
2024-01-02 09:13:26 -0800

    Updated tool page further by adding icons

--- src/tools/Home.jsx
+++ src/tools/Home.jsx
@@ -2,6 +2,11 @@ import React from "react"
 import { Link } from "react-router-dom"
 import Heading from "../common/Heading.jsx"
 import NewIndicator from "../homepage/NewIndicator.jsx"
+import { IoCalculatorSharp } from "react-icons/io5";
+import { MdFormatIndentIncrease } from "react-icons/md";
+import { FiFileText } from "react-icons/fi";
+import { MdOutlineFactCheck } from "react-icons/md";
+
 
 import "./Home.css"
 
@@ -10,18 +15,19 @@ const Home = () => {
     <div className='grid home'>
       <Heading
         type={1}
-        style={{ marginBottom: "3em" }}
         headingText='HMDA Tools'
         paragraphText='Here you can find various tools to assist you in getting your HMDA LAR ready for filing.'
       />
 
       <div className='card-container'>
         <Link to='/tools/rate-spread' className='card tools-card'>
+          <IoCalculatorSharp className="icon"/>
           <Heading
             headingText='Rate Spread Calculator'
             paragraphText='Provides rate spreads for HMDA reportable loans
             with a final action date on or after January 1st, 2018.'
             type={3}
+            style={{ marginTop: 0 }}
           />
         </Link>
 
@@ -29,10 +35,12 @@ const Home = () => {
           className='card tools-card tools-card-no-hover-change'
           style={{ cursor: "initial" }}
         >
+          <MdFormatIndentIncrease className="icon"/>
           <Heading
             headingText='LAR Formatting'
             paragraphText='Tools to help small financial institutions create electronic HMDA submission files.'
             type={3}
+            style={{ marginTop: 0}}
           >
             <ul>
               <li>
@@ -49,18 +57,22 @@ const Home = () => {
         </div>
 
         <Link to='/tools/file-format-verification' className='card tools-card'>
+          <FiFileText className="icon" />
           <Heading
             headingText='File Format Verification Tool'
             paragraphText='Checks if your LAR file meets format specified in the HMDA Filing Instructions Guide.'
             type={3}
+            style={{ marginTop: 0}}
           />
         </Link>
 
         <Link to='/tools/check-digit' className='card tools-card'>
+          <MdOutlineFactCheck className="icon" />
           <Heading
             headingText='Check Digit'
             paragraphText='Generates a two character check digit for a Legal Entity Identifier (LEI) and loan/application ID. It can also validate check digits in a complete Universal Loan Identifier (ULI) that is entered.'
             type={3}
+            style={{ marginTop: 0}}
           />
         </Link>
       </div>
