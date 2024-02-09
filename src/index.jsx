f33202eb600a7edd0b93a32798eb90f09a0d85c0
michaeldremy <michaeldremy@gmail.com>
2024-02-09 08:32:03 -0800

    Upgraded React to v18 and Vite to 4.5.1 to remove a CVE

--- src/index.jsx
+++ src/index.jsx
@@ -7,14 +7,16 @@ window.WritableStream = WritableStream
 window.ReadableStream = ReadableStream
 
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
-ReactDOM.render(
+const container = document.getElementById('root')
+const root = createRoot(container)
+
+root.render(
   <BrowserRouter basename='/'>
     <App />
   </BrowserRouter>,
-  document.getElementById('root'),
 )
