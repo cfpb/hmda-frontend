aa990b17102fd70b965d75b58dfef9ce9787609c
Bill Himmelsbach <whimmels@gmail.com>
2025-06-12 08:59:00 -0700

    fix(data-publication): tweak the langauge around hmda panel

--- src/data-publication/constants/overrides.jsx
+++ src/data-publication/constants/overrides.jsx
@@ -2,8 +2,8 @@ import Alert from "../../common/Alert"
 
 export const Overrides = {
   reporterPanelUnavailableBanner: (
-    <Alert heading={'HMDA Reporter Panel 2024 Unavailable'} type={'info'}>
-      <p>Reporter Panel is unavailable for the 2024 dataset. For institution information refer to the Transmittal Sheet.</p>
+    <Alert heading={'HMDA Reporter Panel for 2024 is Unavailable'} type={'warning'}>
+      <p>HMDA Reporter Panel is unavailable for 2024. Please reference 2024 Transmittal Sheet data for more details regarding identifying institution information.</p>
     </Alert>
   ),
   reporterPanelUnavailable: (
