1d1c03d496c086d79be617b9f2920aa59ad9a483
Bill Himmelsbach <william.himmelsbach@cfpb.gov>
2025-10-06 08:20:38 -0700

    feat(fig): update doc links for 2026

--- src/homepage/ForFilers/FilingGuides.jsx
+++ src/homepage/ForFilers/FilingGuides.jsx
@@ -28,17 +28,16 @@ export function FilingGuides() {
           </a>
           <ul>
             <li>
-              <a href='/documentation/fig/2024/overview'>2024 Online FIG</a>
+              <a href='/documentation/fig/2025/overview'>2025 Online FIG</a>
             </li>
             <li>
-              <a href='/documentation/fig/2025/overview'>2025 Online FIG</a>
+              <a href='/documentation/fig/2026/overview'>2026 Online FIG</a>
               <NewIndicator />
             </li>
             <li>
               <a href='/documentation/fig/2025/supplemental-guide-for-quarterly-filers'>
                 Online Supplemental Guide for Quarterly Filers for 2025
               </a>
-              <NewIndicator />
             </li>
             <li>
               <ExternalLink url='https://www.ffiec.gov/hmda/fileformats.htm'>
