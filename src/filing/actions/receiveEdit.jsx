c3b376e0873696b16960126c89f65aa6383882fd
Chris Contolini <contolini@users.noreply.github.com>
2025-10-22 14:46:56 -0400

    feat: add highlighting to Q659-2 edit
    The Q659 quality edit highlights incorrect values in fields that accept "Not
    applicable" values (specified with a 8888 value). I.e. it flags when you're
    one digit off (888 or 88888).
    
    The highlighting logic has been improved to include values with any number of
    trailing zeros (formerly, it only caught a single trailing zero). I also added
    functionality to highlight the second part of the edit, Q659-2, which is the
    same but with 9s.
    
    See https://ffiec.cfpb.gov/documentation/fig/2025/overview#table7-Q659
    See https://github.local/HMDA-Operations/hmda-devops/issues/5158

--- src/filing/actions/receiveEdit.jsx
+++ src/filing/actions/receiveEdit.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
 import * as types from '../constants'
 
 const PATTERN_1111 = /^1111(\.0)?$/
-const PATTERN_888_8888 = /^888(\.0)?$|^88888(\.0)?$/
+const PATTERN_888_8888 = /^888(\.0+)?$|^88888(\.0+)?$/
+const PATTERN_999_9999 = /^999(\.0+)?$|^99999(\.0+)?$/
 
 /**
  *  Highlight fields whose value matches a pattern
@@ -39,6 +39,11 @@ const TRANSFORMERS = {
     type: 'regex',
     filter: highlightEditFields,
     pattern: PATTERN_888_8888,
+  },
+  'Q659-2': {
+    type: 'regex',
+    filter: highlightEditFields,
+    pattern: PATTERN_999_9999,
   },
 }
 
