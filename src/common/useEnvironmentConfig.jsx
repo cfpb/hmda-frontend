0a11a91755c973a989609a78e8dfc0b1e50bbc39
Bill Himmelsbach <william.himmelsbach@cfpb.gov>
2025-12-30 16:40:34 -0800

    spike(filing): set day one back, and pull from local config

--- src/common/useEnvironmentConfig.jsx
+++ src/common/useEnvironmentConfig.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect } from 'react'
-import { fetchEnvConfig, getDefaultConfig } from './configUtils'
+import { useState } from 'react'
+import { getDefaultConfig } from './configUtils'
 
 export function useEnvironmentConfig(host) {
   const [config, setConfig] = useState(getDefaultConfig(host))
 
-  useEffect(() => {
-    if (window.location.hostname !== 'localhost') {
-      fetchEnvConfig(setConfig, host).catch(() => null)
-    }
-  }, [host])
+  // useEffect(() => {
+  //   if (window.location.hostname !== 'localhost') {
+  //     fetchEnvConfig(setConfig, host).catch(() => null)
+  //   }
+  // }, [host])
 
   return config
 }
