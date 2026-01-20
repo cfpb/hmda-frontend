46d69adc2406e17ca62e56a37a9a87acaf1c7cb6
Bill Himmelsbach <william.himmelsbach@cfpb.gov>
2026-01-20 14:31:38 -0800

    fix(fig): set default to 2025

--- src/homepage/QuickLinks.jsx
+++ src/homepage/QuickLinks.jsx
@@ -69,7 +69,7 @@ export function QuickLinks({ hideContent }) {
             Questions
           </span>
         </a>
-        <a href='/documentation/fig/2026/overview'>
+        <a href='/documentation/fig/2025/overview'>
           <svg
             className='usa-icon'
             aria-hidden='true'
