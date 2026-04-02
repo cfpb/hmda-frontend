c368ea879204de18738fd6221b41e9ac7efe2c25
contolini <contolini@users.noreply.github.com>
2026-04-02 07:45:25 +0000

    Remove preview for PR 2716 🛬

--- pr-preview/pr-2716/assets/NewIndicator-DWmKe980.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-C_pIaBLe.js";function t({text:n,id:r}){return e.jsx("span",{id:r,className:"new-indicator",children:n||"New"})}export{t as N};
--- pr-preview/pr-2716/assets/react-select.browser.esm-D0XJoXEb.js
+++
@@ -1,1 +0,0 @@
-import"./index-C_pIaBLe.js";import{m as a,S as e}from"./stateManager-2f2b6f5b.browser.esm-BM1v4Wza.js";var r=a(e);export{r as i};
