6cbe40d16d9c1f5e60fa86d08cc2960d20201f6a
billhimmelsbach <billhimmelsbach@users.noreply.github.com>
2026-06-23 15:40:16 +0000

    Remove preview for PR 2791 🛬

--- pr-preview/pr-2791/assets/NewIndicator-BFZZqfKh.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index-jcNUzvvj.js";function t({text:n,id:r}){return e.jsx("span",{id:r,className:"new-indicator",children:n||"New"})}export{t as N};
--- pr-preview/pr-2791/assets/react-select.esm-BonsvHAA.js
+++
@@ -1,1 +0,0 @@
-import{u as s,S as o,_ as S}from"./Select-ef7c0426.esm-vs7JPRGM.js";import{O as e}from"./index-jcNUzvvj.js";var n=e.forwardRef(function(a,t){var r=s(a);return e.createElement(o,S({ref:t},r))}),l=n;export{l as S};
