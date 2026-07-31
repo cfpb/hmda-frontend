4dd3358e2db56b6c2b8ac65d29e162e60a3b2e1e
contolini <contolini@users.noreply.github.com>
2026-07-31 15:48:40 +0000

    Deploy preview for PR 2821 🛫

--- pr-preview/pr-2821/assets/NewIndicator-IcZXvyYl.js
+++
@@ -1,1 +0,0 @@
-import{E as e}from"./index-BC7VHRI1.js";function t({text:n,id:r}){return e.jsx("span",{id:r,className:"new-indicator",children:n||"New"})}export{t as N};
--- pr-preview/pr-2821/assets/index-CzcWa7eB.js
+++ pr-preview/pr-2821/assets/index-CzcWa7eB.js
@@ -1,10 +1,8 @@
-import{O as reactExports,s as getDefaultExportFromCjs,E as jsxRuntimeExports,z as iconSprite,o as commonjsRequire,G as ordinal,aB as useHistory,aC as useLocation,S as Switch,j as Route,c as LoadingIcon}from"./index-BC7VHRI1.js";import{B as Buffer}from"./index-CgqXENQe.js";function useRemoteMarkdown(e,n={}){const[t,r]=reactExports.useState(n.defaultData),[i,a]=reactExports.useState(!1),[l,p]=reactExports.useState(null),{forceFetch:f}=n,c=f||window.location.host.indexOf("localhost")<0;return reactExports.useEffect(()=>{c&&(a(!0),p(null),fetch(e).then(u=>u.ok?u.text():Promise.reject(u)).then(u=>{n.transformReceive?r(n.transformReceive(u)):r(u),a(!1)}).catch(u=>{const{status:s,statusText:h}=u;a(!1),p(n.errorMsg||`Error: ${s} - ${h}`)}))},[]),[t,i,l]}const defaultChangeLog=`---
+import{O as reactExports,s as getDefaultExportFromCjs,E as jsxRuntimeExports,z as iconSprite,o as commonjsRequire,G as ordinal,aB as useHistory,aC as useLocation,S as Switch,j as Route,c as LoadingIcon}from"./index-BdKTKN8k.js";import{B as Buffer}from"./index-CgqXENQe.js";function useRemoteMarkdown(e,n={}){const[t,r]=reactExports.useState(n.defaultData),[i,a]=reactExports.useState(!1),[l,p]=reactExports.useState(null),{forceFetch:f}=n,c=f||window.location.host.indexOf("localhost")<0;return reactExports.useEffect(()=>{c&&(a(!0),p(null),fetch(e).then(u=>u.ok?u.text():Promise.reject(u)).then(u=>{n.transformReceive?r(n.transformReceive(u)):r(u),a(!1)}).catch(u=>{const{status:s,statusText:h}=u;a(!1),p(n.errorMsg||`Error: ${s} - ${h}`)}))},[]),[t,i,l]}const defaultChangeLog=`---
 date: 07/31/26
 type: announcement
 product: tools
 ---
-### News and Updates 
-
 For 2024 data and later, the Institutions API will be populated with data from the HMDA Transmittal Sheet, instead of the HMDA Panel. This change was made to ensure that data provided by the Institution API are current since the HMDA Panel was discontinued. 
 
 The HMDA Panel contained more fields than the HMDA Transmittal Sheet. Fields not available from the HMDA Transmittal Sheet will return as default empty values. Data from 2023 and earlier is populated from the HMDA Panel and all fields in the API are used where data was relevant and available. 
@@ -745,7 +743,7 @@ type: release
 product: documentation
 ---
 The Documentation for 2020 Annual and Quarterly filing deadlines has been released.
-`,PRODUCT_NAMES={mlar:"Modified LAR",datasets:"National Datasets",reports:"Reports",documentation:"Documentation",tools:"HMDA Tools",filing:"HMDA Filing"},PRODUCTS=Object.keys(PRODUCT_NAMES),CATEGORIES={correction:{order:4},update:{order:3},release:{order:2},announcement:{order:1}},PUB_CHANGELOG_URL="https://raw.githubusercontent.com/cfpb/hmda-frontend/53e3f243010570663e90b8c5c4ef1e4258adb9e4/src/updates-notes/change-log.md",DEFAULT_FILTERS={type:[],product:[],keywords:[]},FILTER_OPTIONS={PRODUCT:PRODUCTS.map(e=>({value:e,type:"product"})),TYPE:Object.keys(CATEGORIES).sort((e,n)=>CATEGORIES[e].order-CATEGORIES[n].order).map(e=>({value:e,type:"type"}))};var mark$3={exports:{}};/*!***************************************************
+`,PRODUCT_NAMES={mlar:"Modified LAR",datasets:"National Datasets",reports:"Reports",documentation:"Documentation",tools:"HMDA Tools",filing:"HMDA Filing"},PRODUCTS=Object.keys(PRODUCT_NAMES),CATEGORIES={correction:{order:4},update:{order:3},release:{order:2},announcement:{order:1}},PUB_CHANGELOG_URL="https://raw.githubusercontent.com/cfpb/hmda-frontend/a3620d672266dbdf2ea06a2c2bc53dbceebc45f8/src/updates-notes/change-log.md",DEFAULT_FILTERS={type:[],product:[],keywords:[]},FILTER_OPTIONS={PRODUCT:PRODUCTS.map(e=>({value:e,type:"product"})),TYPE:Object.keys(CATEGORIES).sort((e,n)=>CATEGORIES[e].order-CATEGORIES[n].order).map(e=>({value:e,type:"type"}))};var mark$3={exports:{}};/*!***************************************************
 * mark.js v8.11.1
 * https://markjs.io/
 * Copyright (c) 2014–2018, Julian Kühnel
